4410c3238303c9fd3e643840a452366de1f77c22
Sylvain Lebresne <sylvain@datastax.com>
2013-02-16 00:13:59 +0100

    Fix setting compression to None

--- driver-core/src/main/java/com/datastax/driver/core/ProtocolOptions.java
+++ driver-core/src/main/java/com/datastax/driver/core/ProtocolOptions.java
@@ -115,7 +115,7 @@ public class ProtocolOptions {
      * available.
      */
     public ProtocolOptions setCompression(Compression compression) {
-        if (compression.compressor == null)
+        if (compression != Compression.NONE && compression.compressor == null)
             throw new IllegalStateException("The requested compression is not available (some compression require a JAR to be found in the classpath)");
 
         this.compression = compression;
