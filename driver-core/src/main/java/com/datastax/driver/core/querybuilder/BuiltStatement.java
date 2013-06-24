apache/cassandra-java-driver

commit 124ac56e21202ec95639191ffcf6a72565ab44da

QueryBuilder: function calls should now use fcall
JAVA-100 #fixes

driver-core/src/main/java/com/datastax/driver/core/querybuilder/BuiltStatement.java
@@ -70,7 +70,7 @@ abstract class BuiltStatement extends Statement {
             return;
 
         for (int i = 0; i < partitionKey.size(); i++) {
-            if (name.equals(partitionKey.get(i).getName()) && !Utils.isFunctionCall(value)) {
+            if (name.equals(partitionKey.get(i).getName()) && !Utils.isRawValue(value)) {
                 routingKey[i] = partitionKey.get(i).getType().parse(Utils.toRawString(value));
                 return;
             }

driver-core/src/main/java/com/datastax/driver/core/querybuilder/Delete.java
@@ -27,11 +27,11 @@ public class Delete extends BuiltStatement {
 
     private final String keyspace;
     private final String table;
-    private final List<String> columnNames;
+    private final List<Object> columnNames;
     private final Where where;
     private final Options usings;
 
-    Delete(String keyspace, String table, List<String> columnNames) {
+    Delete(String keyspace, String table, List<Object> columnNames) {
         super();
         this.keyspace = keyspace;
         this.table = table;
@@ -40,7 +40,7 @@ public class Delete extends BuiltStatement {
         this.usings = new Options(this);
     }
 
-    Delete(TableMetadata table, List<String> columnNames) {
+    Delete(TableMetadata table, List<Object> columnNames) {
         super(table);
         this.keyspace = table.getKeyspace().getName();
         this.table = table.getName();
@@ -180,11 +180,11 @@ public class Delete extends BuiltStatement {
      */
     public static class Builder {
 
-        protected List<String> columnNames;
+        protected List<Object> columnNames;
 
         protected Builder() {}
 
-        Builder(List<String> columnNames) {
+        Builder(List<Object> columnNames) {
             this.columnNames = columnNames;
         }
 
@@ -247,7 +247,7 @@ public class Delete extends BuiltStatement {
          */
         public Selection column(String name) {
             if (columnNames == null)
-                columnNames = new ArrayList<String>();
+                columnNames = new ArrayList<Object>();
 
             columnNames.add(name);
             return this;

driver-core/src/main/java/com/datastax/driver/core/querybuilder/Insert.java
@@ -28,7 +28,7 @@ public class Insert extends BuiltStatement {
 
     private final String keyspace;
     private final String table;
-    private final List<String> names = new ArrayList<String>();
+    private final List<Object> names = new ArrayList<Object>();
     private final List<Object> values = new ArrayList<Object>();
     private final Options usings;
 

driver-core/src/main/java/com/datastax/driver/core/querybuilder/QueryBuilder.java
@@ -55,7 +55,7 @@ public final class QueryBuilder {
      * least a FROM clause to complete the query).
      */
     public static Select.Builder select(String... columns) {
-        return new Select.Builder(Arrays.asList(columns));
+        return new Select.Builder(Arrays.asList((Object[])columns));
     }
 
     /**
@@ -141,7 +141,7 @@ public final class QueryBuilder {
      * clause needs to be provided to complete the query).
      */
     public static Delete.Builder delete(String... columns) {
-        return new Delete.Builder(Arrays.asList(columns));
+        return new Delete.Builder(Arrays.asList((Object[])columns));
     }
 
     /**
@@ -232,7 +232,7 @@ public final class QueryBuilder {
     public static String token(String... columnNames) {
         StringBuilder sb = new StringBuilder();
         sb.append("token(");
-        Utils.joinAndAppendNames(sb, ",", Arrays.asList(columnNames));
+        Utils.joinAndAppendNames(sb, ",", Arrays.asList((Object[])columnNames));
         sb.append(")");
         return sb.toString();
     }
@@ -633,6 +633,30 @@ public final class QueryBuilder {
      * @return the value but protected from being interpreted/escaped by the query builder.
      */
     public static Object raw(String str) {
-        return new Utils.RawString(str);
+        return new Utils.RawString("'" + str + "'");
+    }
+
+    /**
+     * Creates a function call.
+     *
+     * @param name the name of the function to call.
+     * @param parameters the paramters for the function.
+     * @return the function call.
+     */
+    public static Object fcall(String name, Object... parameters) {
+        return new Utils.FCall(name, parameters);
+    }
+
+    /**
+     * Declares that the name in argument should be treated as a column name.
+     * <p>
+     * This mainly meant for use with {@link Select.Builder#fcall} when a
+     * function should apply to a column name, not a string value.
+     *
+     * @param name the name of the column.
+     * @return the name as a column name.
+     */
+    public static Object column(String name) {
+        return new Utils.CName(name);
     }
 }

driver-core/src/main/java/com/datastax/driver/core/querybuilder/Select.java
@@ -27,17 +27,17 @@ import com.datastax.driver.core.TableMetadata;
  */
 public class Select extends BuiltStatement {
 
-    private static final List<String> COUNT_ALL = Collections.singletonList("count(*)");
+    private static final List<Object> COUNT_ALL = Collections.<Object>singletonList(new Utils.FCall("count", new Utils.RawString("*")));
 
     private final String keyspace;
     private final String table;
-    private final List<String> columnNames;
+    private final List<Object> columnNames;
     private final Where where;
     private List<Ordering> orderings;
     private int limit = -1;
     private boolean allowFiltering;
 
-    Select(String keyspace, String table, List<String> columnNames) {
+    Select(String keyspace, String table, List<Object> columnNames) {
         super();
         this.keyspace = keyspace;
         this.table = table;
@@ -45,7 +45,7 @@ public class Select extends BuiltStatement {
         this.where = new Where(this);
     }
 
-    Select(TableMetadata table, List<String> columnNames) {
+    Select(TableMetadata table, List<Object> columnNames) {
         super(table);
         this.keyspace = table.getKeyspace().getName();
         this.table = table.getName();
@@ -220,11 +220,11 @@ public class Select extends BuiltStatement {
      */
     public static class Builder {
 
-        protected List<String> columnNames;
+        protected List<Object> columnNames;
 
         protected Builder() {}
 
-        Builder(List<String> columnNames) {
+        Builder(List<Object> columnNames) {
             this.columnNames = columnNames;
         }
 
@@ -294,6 +294,14 @@ public class Select extends BuiltStatement {
             return (Builder)this;
         }
 
+        private Selection addName(Object name) {
+            if (columnNames == null)
+                columnNames = new ArrayList<Object>();
+
+            columnNames.add(name);
+            return this;
+        }
+
         /**
          * Selects the provided column.
          *
@@ -301,39 +309,46 @@ public class Select extends BuiltStatement {
          * @return this in-build SELECT statement
          */
         public Selection column(String name) {
-            if (columnNames == null)
-                columnNames = new ArrayList<String>();
-
-            columnNames.add(name);
-            return this;
+            return addName(name);
         }
 
         /**
          * Selects the write time of provided column.
+         * <p>
+         * This is a shortcut for {@code fcall("writetime", QueryBuilder.column(name))}.
          *
          * @param name the name of the column to select the write time of.
          * @return this in-build SELECT statement
          */
         public Selection writeTime(String name) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("writetime(");
-            Utils.appendName(name, sb);
-            sb.append(")");
-            return column(sb.toString());
+            return addName(new Utils.FCall("writetime", new Utils.CName(name)));
         }
 
         /**
          * Selects the ttl of provided column.
+         * <p>
+         * This is a shortcut for {@code fcall("ttl", QueryBuilder.column(name))}.
          *
          * @param name the name of the column to select the ttl of.
          * @return this in-build SELECT statement
          */
         public Selection ttl(String name) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("ttl(");
-            Utils.appendName(name, sb);
-            sb.append(")");
-            return column(sb.toString());
+            return addName(new Utils.FCall("ttl", new Utils.CName(name)));
+        }
+
+        /**
+         * Creates a function call.
+         * <p>
+         * Please note that the parameters are interpreted as values, and so
+         * {@code fcall("textToBlob", "foo")} will generate the string
+         * {@code "textToBlob('foo')"}. If you want to generate
+         * {@code "textToBlob(foo)"}, i.e. if the argument must be interpreted
+         * as a column name (in a select clause), you will need to use the
+         * {@link QueryBuilder#column} method, and so 
+         * {@code fcall("textToBlob", QueryBuilder.column(foo)}.
+         */
+        public Selection fcall(String name, Object... parameters) {
+            return addName(new Utils.FCall(name, parameters));
         }
     }
 }

driver-core/src/main/java/com/datastax/driver/core/querybuilder/Utils.java
@@ -26,7 +26,6 @@ import org.apache.cassandra.utils.ByteBufferUtil;
 abstract class Utils {
 
     private static final Pattern cnamePattern = Pattern.compile("\\w+(?:\\[.+\\])?");
-    private static final Pattern fctsPattern = Pattern.compile("\\s*[a-zA-Z]\\w*\\(.+");
 
     static StringBuilder joinAndAppend(StringBuilder sb, String separator, List<? extends Appendeable> values) {
         for (int i = 0; i < values.size(); i++) {
@@ -37,7 +36,7 @@ abstract class Utils {
         return sb;
     }
 
-    static StringBuilder joinAndAppendNames(StringBuilder sb, String separator, List<String> values) {
+    static StringBuilder joinAndAppendNames(StringBuilder sb, String separator, List<Object> values) {
         for (int i = 0; i < values.size(); i++) {
             if (i > 0)
                 sb.append(separator);
@@ -72,21 +71,17 @@ abstract class Utils {
         if (appendValueIfCollection(value, sb, rawValue))
             return sb;
 
-        if (rawValue || isFunctionCall(value) || value instanceof RawString)
+        if (rawValue || value instanceof RawString)
             return sb.append(value.toString());
         else
             return appendValueString(value.toString(), sb);
-    }
-
-    static boolean isFunctionCall(Object value) {
-        return value instanceof String && fctsPattern.matcher((String)value).matches();
     }
 
     private static void appendFlatValue(Object value, StringBuilder sb, boolean rawValue) {
         if (appendValueIfLiteral(value, sb))
             return;
 
-        if (rawValue || isFunctionCall(value) || value instanceof RawString)
+        if (rawValue || value instanceof RawString)
             sb.append(value.toString());
         else
             appendValueString(value.toString(), sb);
@@ -108,6 +103,19 @@ abstract class Utils {
             return true;
         } else if (value == QueryBuilder.BIND_MARKER) {
             sb.append("?");
+            return true;
+        } else if (value instanceof FCall) {
+            FCall fcall = (FCall)value;
+            sb.append(fcall.name).append("(");
+            for (int i = 0; i < fcall.parameters.length; i++) {
+                if (i > 0)
+                    sb.append(",");
+                appendValue(fcall.parameters[i], sb);
+            }
+            sb.append(")");
+            return true;
+        } else if (value instanceof CName) {
+            appendName(((CName)value).name, sb);
             return true;
         } else if (value == null) {
             sb.append("null");
@@ -192,16 +200,44 @@ abstract class Utils {
         return sb.append("'").append(replace(value, '\'', "''")).append("'");
     }
 
+    static boolean isRawValue(Object value) {
+        return value != null
+            && !(value instanceof FCall)
+            && !(value instanceof CName)
+            && value != QueryBuilder.BIND_MARKER;
+    }
+
     static String toRawString(Object value) {
         return appendValue(value, new StringBuilder(), true).toString();
     }
 
     static StringBuilder appendName(String name, StringBuilder sb) {
         name = name.trim();
-        if (cnamePattern.matcher(name).matches() || name.startsWith("\"") || fctsPattern.matcher(name).matches())
+        // FIXME: checking for token( specifically is uber ugly, we'll need some better solution.
+        if (cnamePattern.matcher(name).matches() || name.startsWith("\"") || name.startsWith("token("))
             sb.append(name);
         else
             sb.append("\"").append(name).append("\"");
+        return sb;
+    }
+
+    static StringBuilder appendName(Object name, StringBuilder sb) {
+        if (name instanceof String) {
+            appendName((String)name, sb);
+        } else if (name instanceof CName) {
+            appendName(((CName)name).name, sb);
+        } else if (name instanceof FCall) {
+            FCall fcall = (FCall)name;
+            sb.append(fcall.name).append("(");
+            for (int i = 0; i < fcall.parameters.length; i++) {
+                if (i > 0)
+                    sb.append(",");
+                appendValue(fcall.parameters[i], sb);
+            }
+            sb.append(")");
+        } else {
+            appendName((String)name, sb);
+        }
         return sb;
     }
 
@@ -250,7 +286,43 @@ abstract class Utils {
 
         @Override
         public String toString() {
-            return "'" + str + "'";
+            return str;
+        }
+    }
+
+    static class FCall {
+        private final String name;
+        private final Object[] parameters;
+
+        FCall(String name, Object... parameters) {
+            this.name = name;
+            this.parameters = parameters;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append(name).append("(");
+            for (int i = 0; i < parameters.length; i++) {
+                if (i > 0)
+                    sb.append(",");
+                sb.append(parameters[i]);
+            }
+            sb.append(")");
+            return sb.toString();
+        }
+    }
+
+    static class CName {
+        private final String name;
+
+        CName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
         }
     }
 }

driver-core/src/test/java/com/datastax/driver/core/querybuilder/QueryBuilderTest.java
@@ -62,16 +62,20 @@ public class QueryBuilderTest {
         // Ensure getQueryString() == to.String()
         assertEquals(select().countAll().from("foo").getQueryString(), query);
 
+        query = "SELECT intToBlob(b) FROM foo;";
+        select = select().fcall("intToBlob", column("b")).from("foo");
+        assertEquals(select.toString(), query);
+
         query = "SELECT * FROM foo WHERE k>42 LIMIT 42;";
         select = select().all().from("foo").where(gt("k", 42)).limit(42);
         assertEquals(select.toString(), query);
 
         query = "SELECT * FROM foo WHERE token(k)>token(42);";
-        select = select().all().from("foo").where(gt(token("k"), token("42")));
+        select = select().all().from("foo").where(gt(token("k"), fcall("token", 42)));
         assertEquals(select.toString(), query);
 
         query = "SELECT * FROM foo2 WHERE token(a,b)>token(42,101);";
-        select = select().all().from("foo2").where(gt(token("a", "b"), token("42", "101")));
+        select = select().all().from("foo2").where(gt(token("a", "b"), fcall("token", 42, 101)));
         assertEquals(select.toString(), query);
 
         try {
@@ -412,7 +416,7 @@ public class QueryBuilderTest {
         assertEquals(select.toString(), query);
 
         query = "SELECT * FROM t WHERE c=now();";
-        select = select().from("t").where(eq("c", "now()"));
+        select = select().from("t").where(eq("c", fcall("now")));
         assertEquals(select.toString(), query);
 
         query = "SELECT * FROM t WHERE c='now()';";

commit 93ebb7451f0ea8daa54e1a50e0cb4f1aafb77954

JAVA-121 #fixes Fix issue introduced in 124ac56e21202ec95639191ffcf6a72565ab44da

driver-core/src/main/java/com/datastax/driver/core/querybuilder/BuiltStatement.java
@@ -70,7 +70,7 @@ abstract class BuiltStatement extends Statement {
             return;
 
         for (int i = 0; i < partitionKey.size(); i++) {
-            if (name.equals(partitionKey.get(i).getName()) && !Utils.isRawValue(value)) {
+            if (name.equals(partitionKey.get(i).getName()) && Utils.isRawValue(value)) {
                 routingKey[i] = partitionKey.get(i).getType().parse(Utils.toRawString(value));
                 return;
             }