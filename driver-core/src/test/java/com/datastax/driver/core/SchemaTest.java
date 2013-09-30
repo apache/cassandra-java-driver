30401de55c697888152b4d83ee1a182c31c6e873
Sylvain Lebresne <sylvain@datastax.com>
2013-09-30 14:04:33 +0200

    Escape identifiers and use FQN for tables in schema export

--- driver-core/src/test/java/com/datastax/driver/core/SchemaTest.java
+++ driver-core/src/test/java/com/datastax/driver/core/SchemaTest.java
@@ -34,7 +34,7 @@ public class SchemaTest extends CCMBridge.PerClassSingleNodeCluster {
     @Override
     protected Collection<String> getTableDefinitions() {
 
-        String sparse = "CREATE TABLE sparse (\n"
+        String sparse = "CREATE TABLE ks.sparse (\n"
                       + "    k text,\n"
                       + "    c1 int,\n"
                       + "    c2 float,\n"
@@ -43,7 +43,7 @@ public class SchemaTest extends CCMBridge.PerClassSingleNodeCluster {
                       + "    PRIMARY KEY (k, c1, c2)\n"
                       + ");";
 
-        String st = "CREATE TABLE static (\n"
+        String st = "CREATE TABLE ks.static (\n"
                   + "    k text,\n"
                   + "    i int,\n"
                   + "    m map<text, timeuuid>,\n"
@@ -51,7 +51,7 @@ public class SchemaTest extends CCMBridge.PerClassSingleNodeCluster {
                   + "    PRIMARY KEY (k)\n"
                   + ");";
 
-        String compactStatic = "CREATE TABLE compact_static (\n"
+        String compactStatic = "CREATE TABLE ks.compact_static (\n"
                              + "    k text,\n"
                              + "    i int,\n"
                              + "    t timeuuid,\n"
@@ -59,14 +59,14 @@ public class SchemaTest extends CCMBridge.PerClassSingleNodeCluster {
                              + "    PRIMARY KEY (k)\n"
                              + ") WITH COMPACT STORAGE;";
 
-        String compactDynamic = "CREATE TABLE compact_dynamic (\n"
+        String compactDynamic = "CREATE TABLE ks.compact_dynamic (\n"
                               + "    k text,\n"
                               + "    c int,\n"
                               + "    v timeuuid,\n"
                               + "    PRIMARY KEY (k, c)\n"
                               + ") WITH COMPACT STORAGE;";
 
-        String compactComposite = "CREATE TABLE compact_composite (\n"
+        String compactComposite = "CREATE TABLE ks.compact_composite (\n"
                                 + "    k text,\n"
                                 + "    c1 int,\n"
                                 + "    c2 float,\n"
@@ -81,7 +81,7 @@ public class SchemaTest extends CCMBridge.PerClassSingleNodeCluster {
         compact.put("compact_dynamic", compactDynamic);
         compact.put("compact_composite", compactComposite);
 
-        withOptions = "CREATE TABLE with_options (\n"
+        withOptions = "CREATE TABLE ks.with_options (\n"
                     + "    k text,\n"
                     + "    i int,\n"
                     + "    PRIMARY KEY (k)\n"
