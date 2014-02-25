14cc5de3b3a26118805b5fcdc67d3336e71fa090
Sylvain Lebresne <sylvain@datastax.com>
2014-02-25 17:42:20 +0100

    Don't connect to nodes that are ignored by the load balancing policy
    JAVA-270 #fixes

--- driver-core/src/main/java/com/datastax/driver/core/Cluster.java
+++ driver-core/src/main/java/com/datastax/driver/core/Cluster.java
@@ -1020,6 +1020,23 @@ public class Cluster {
             if (isShutdown.get())
                 return;
 
+            // Adds to the load balancing first and foremost, as doing so might change the decision
+            // it will make for distance() on that node (not likely but we leave that possibility).
+            // This does mean the policy may start returning that node for query plan, but as long
+            // as no pools have been created (below) this will be ignored by RequestHandler so it's fine.
+            loadBalancingPolicy().onAdd(host);
+
+            // Next, if the host should be ignored, well, ignore it.
+            if (loadBalancingPolicy().distance(host) == HostDistance.IGNORED) {
+                // We still mark the node UP though as it should be (and notifiy the listeners).
+                // We'll mark it down if we have  a notification anyway and we've documented that especially
+                // for IGNORED hosts, the isUp() method was a best effort guess
+                host.setUp();
+                for (Host.StateListener listener : listeners)
+                    listener.onAdd(host);
+                return;
+            }
+
             try {
                 prepareAllQueries(host);
             } catch (InterruptedException e) {
@@ -1027,7 +1044,6 @@ public class Cluster {
                 // Don't propagate because we don't want to prevent other listener to run
             }
 
-            loadBalancingPolicy().onAdd(host);
             controlConnection.onAdd(host);
 
             List<ListenableFuture<Boolean>> futures = new ArrayList<ListenableFuture<Boolean>>(sessions.size());
