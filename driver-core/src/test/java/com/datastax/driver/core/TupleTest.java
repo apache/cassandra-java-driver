3a696050180b3dd98b7c4f05624419f90e411d0f
Joaquin Casares <joaquin@datastax.com>
2014-07-25 11:44:53 -0500

    Fix trivial test bugs

--- driver-core/src/test/java/com/datastax/driver/core/TupleTest.java
+++ driver-core/src/test/java/com/datastax/driver/core/TupleTest.java
@@ -315,7 +315,7 @@ public class TupleTest extends CCMBridge.PerClassSingleNodeCluster {
             session.execute(String.format("CREATE TABLE mytable (k int PRIMARY KEY, %s)", Joiner.on(',').join(values)));
 
 
-            int i = 1;
+            int i = 0;
             // test tuple<list<datatype>>
             for (DataType datatype : DATA_TYPE_PRIMITIVES) {
                 // create tuple
@@ -344,7 +344,7 @@ public class TupleTest extends CCMBridge.PerClassSingleNodeCluster {
                 ArrayList<DataType> dataTypes = new ArrayList<DataType>();
                 ArrayList<Object> createdValues = new ArrayList<Object>();
 
-                dataTypes.add(DataType.list(datatype));
+                dataTypes.add(DataType.set(datatype));
                 createdValues.add(new HashSet<Object>(Arrays.asList(SAMPLE_DATA.get(datatype))));
 
                 TupleType t = new TupleType(dataTypes);
@@ -369,7 +369,7 @@ public class TupleTest extends CCMBridge.PerClassSingleNodeCluster {
                 HashMap<Object, Object> hm = new HashMap<Object, Object>();
                 hm.put(SAMPLE_DATA.get(datatype), SAMPLE_DATA.get(datatype));
 
-                dataTypes.add(DataType.list(datatype));
+                dataTypes.add(DataType.map(datatype, datatype));
                 createdValues.add(hm);
 
                 TupleType t = new TupleType(dataTypes);
