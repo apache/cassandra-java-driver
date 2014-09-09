1e259f7246157e3fc7dac6c2cb3fc453fd5bd7ef
olim7t <omichallat+github@gmail.com>
2014-09-09 09:44:52 -0700

    Fix bugs in @Frozen checks (JAVA-441).
    * include description of cause in error message
    * don't expect enums to be frozen

--- driver-mapping/src/main/java/com/datastax/driver/mapping/AnnotationChecks.java
+++ driver-mapping/src/main/java/com/datastax/driver/mapping/AnnotationChecks.java
@@ -93,7 +93,7 @@ class AnnotationChecks {
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(String.format("Error while checking frozen types on field %s of %s %s: %s",
                                                              field.getName(), classDescription,
-                                                             field.getDeclaringClass().getName()));
+                                                             field.getDeclaringClass().getName(), e.getMessage()));
         }
     }
 
@@ -172,7 +172,7 @@ class AnnotationChecks {
     }
 
     private static void checkValidFrozen(Class<?> clazz, boolean declared) {
-        boolean expected = !EXPECTED_NON_FROZEN_CLASSES.contains(clazz);
+        boolean expected = !EXPECTED_NON_FROZEN_CLASSES.contains(clazz) && !clazz.isEnum();
         if (expected != declared)
             throw new IllegalArgumentException(String.format("expected %s to be %sfrozen but was %sfrozen",
                                                              clazz.getSimpleName(),
