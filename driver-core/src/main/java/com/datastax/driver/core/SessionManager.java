9e5ee52242667d0d879131fa86636855e3dde16e
olim7t <omichallat+github@gmail.com>
2014-10-31 12:53:34 +0100

    Parallelize pool creations when creating the first session (JAVA-522).

--- driver-core/src/main/java/com/datastax/driver/core/SessionManager.java
+++ driver-core/src/main/java/com/datastax/driver/core/SessionManager.java
@@ -64,7 +64,39 @@ class SessionManager extends AbstractSession {
         // If we haven't initialized the cluster, do it now
         cluster.init();
 
-        // Create pool to initial nodes (and wait for them to be created)
+        // Create pools to initial nodes (and wait for them to be created)
+        Collection<Host> hosts = cluster.getMetadata().allHosts();
+        if (cluster.manager.sessions.size() == 1) {
+            // We only do it in parallel if this is the first session (meaning that the cluster just initialized).
+            createPoolsInParallel(hosts);
+        } else {
+            // Otherwise, we don't want to fill executor() because this is also where up/down notifications are processed,
+            // it's important that existing sessions get them in a timely manner. So we create the pools one by one:
+            createPoolsSequentially(hosts);
+        }
+
+        isInit = true;
+        updateCreatedPools(executor());
+        return this;
+    }
+
+    private void createPoolsInParallel(Collection<Host> hosts) {
+        List<ListenableFuture<Boolean>> futures = new ArrayList<ListenableFuture<Boolean>>(hosts.size());
+        for (Host host : hosts)
+            if (host.state != Host.State.DOWN)
+                futures.add(maybeAddPool(host, executor()));
+        ListenableFuture<List<Boolean>> f = Futures.allAsList(futures);
+        try {
+            f.get();
+        } catch (ExecutionException e) {
+            // This is not supposed to happen
+            throw new DriverInternalError(e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void createPoolsSequentially(Collection<Host> hosts) {
         for (Host host : cluster.getMetadata().allHosts()) {
             try {
                 if (host.state != Host.State.DOWN)
@@ -76,9 +108,6 @@ class SessionManager extends AbstractSession {
                 Thread.currentThread().interrupt();
             }
         }
-        isInit = true;
-        updateCreatedPools(executor());
-        return this;
     }
 
     public String getLoggedKeyspace() {
