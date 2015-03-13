1ffcf34398f79ea283cf74ed38b3d6c93bac13c8
Alexander Dejanovski <adejanovski@gmail.com>
2015-03-13 18:33:25 +0100

    New class for Metadata Result sets, in order to avoid using core's ColumnDefinition class

--- driver-jdbc/src/main/java/com/datastax/driver/jdbc/MetadataRow.java
+++ driver-jdbc/src/main/java/com/datastax/driver/jdbc/MetadataRow.java
@@ -17,15 +17,18 @@ import java.util.UUID;
 
 
 
-import com.datastax.driver.core.ColumnDefinitions;
-import com.datastax.driver.core.ColumnDefinitions.Definition;
+
+
 import com.datastax.driver.core.DataType;
 import com.datastax.driver.core.TupleValue;
 import com.datastax.driver.core.UDTValue;
+import com.datastax.driver.jdbc.ColumnDefinitions.Definition;
+import com.datastax.driver.jdbc.ColumnDefinitions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-public class MetadataRow implements com.datastax.driver.core.Row{
+//public class MetadataRow implements com.datastax.driver.core.Row{
+public class MetadataRow{
 	
 	private ArrayList<String> entries;
 	private HashMap<String,Integer> names;
@@ -48,31 +51,31 @@ public class MetadataRow implements com.datastax.driver.core.Row{
 	}
 	
 	
-	@Override
+	
 	public UDTValue getUDTValue(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public TupleValue getTupleValue(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public UDTValue getUDTValue(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public TupleValue getTupleValue(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public ColumnDefinitions getColumnDefinitions() {
 		// TODO Auto-generated method stub
 		Definition[] definitionArr = new Definition[definitions.size()];
@@ -82,206 +85,206 @@ public class MetadataRow implements com.datastax.driver.core.Row{
 		return new ColumnDefinitions(definitionArr);
 	}
 
-	@Override
+
 	public boolean isNull(int i) {
 		// TODO Auto-generated method stub
 		return entries.get(i)==null;
 	}
 
-	@Override
+
 	public boolean isNull(String name) {
 		// TODO Auto-generated method stub
 		return isNull(names.get(name));
 	}
 
-	@Override
+ 
 	public boolean getBool(int i) {
 		// TODO Auto-generated method stub
 		return Boolean.parseBoolean(entries.get(i));
 	}
 
-	@Override
+ 
 	public boolean getBool(String name) {
 		// TODO Auto-generated method stub
 		return getBool(names.get(name));
 	}
 
-	@Override
+ 
 	public int getInt(int i) {
 		// TODO Auto-generated method stub
 		return Integer.parseInt(entries.get(i));
 	}
 
-	@Override
+ 
 	public int getInt(String name) {		
 		return getInt(names.get(name));
 	}
 
-	@Override
+ 
 	public long getLong(int i) {
 		// TODO Auto-generated method stub
 		return Long.parseLong(entries.get(i));
 	}
 
-	@Override
+ 
 	public long getLong(String name) {
 		// TODO Auto-generated method stub
 		return getLong(names.get(name));
 	}
 
-	@Override
+ 
 	public Date getDate(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public Date getDate(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public float getFloat(int i) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+ 
 	public float getFloat(String name) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+ 
 	public double getDouble(int i) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+ 
 	public double getDouble(String name) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+ 
 	public ByteBuffer getBytesUnsafe(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public ByteBuffer getBytesUnsafe(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public ByteBuffer getBytes(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public ByteBuffer getBytes(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public String getString(int i) {
 		// TODO Auto-generated method stub
 		
 		return entries.get(i);
 	}
 
-	@Override
+ 
 	public String getString(String name) {
 		// TODO Auto-generated method stub
 		return getString(names.get(name));
 	}
 
-	@Override
+ 
 	public BigInteger getVarint(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public BigInteger getVarint(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public BigDecimal getDecimal(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public BigDecimal getDecimal(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public UUID getUUID(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public UUID getUUID(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public InetAddress getInet(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public InetAddress getInet(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public <T> List<T> getList(int i, Class<T> elementsClass) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public <T> List<T> getList(String name, Class<T> elementsClass) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public <T> Set<T> getSet(int i, Class<T> elementsClass) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public <T> Set<T> getSet(String name, Class<T> elementsClass) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public <K, V> Map<K, V> getMap(int i, Class<K> keysClass,
 			Class<V> valuesClass) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+ 
 	public <K, V> Map<K, V> getMap(String name, Class<K> keysClass,
 			Class<V> valuesClass) {
 		// TODO Auto-generated method stub
