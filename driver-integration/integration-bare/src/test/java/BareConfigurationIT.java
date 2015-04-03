d02485e5c2273c7b7856d36b5a2f51e6a39f9aea
Andrew Tolbert <andrew.tolbert@datastax.com>
2015-04-02 19:02:46 -0500

    Add driver-integration for testing with different package configs.
    Since the behavior of the driver may be altered based on libraries that
    are present we need a mechanism for testing with these configurations.
    The 'osgi' example provides most of this functionality as it loads up
    OSGi containers with different library configurations, but this not
    adequate in some cases, particularly the 'shaded' case where some
    classes use a different packaging namespace.

---
+++ driver-integration/integration-bare/src/test/java/BareConfigurationIT.java
@@ -0,0 +1,84 @@
+/*
+ *      Copyright (C) 2012-2015 DataStax Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.NettyOptions;
+import com.datastax.driver.core.ProtocolOptions;
+import com.datastax.driver.integration.BaseIntegrationTest;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.DefaultThreadFactory;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BareConfigurationIT extends BaseIntegrationTest {
+
+    /**
+     * <p>
+     * Ensures that if LZ4 is not in the classpath that it cannot be used for compression.
+     *
+     * @test_category packaging, connection:compression
+     * @expected_result An exception is thrown at the time the cluster is built.
+     */
+    @Test(groups="unit", expectedExceptions=IllegalStateException.class,
+            expectedExceptionsMessageRegExp = "The requested compression is not available.*")
+    public void should_not_be_able_to_use_lz4() {
+        configure(Cluster.builder())
+                .addContactPointsWithPorts(Collections.singletonList(hostAddress)).
+                withCompression(ProtocolOptions.Compression.LZ4).build();
+    }
+
+    /**
+     * <p>
+     * Ensures that if Snappy is not in the classpath that it cannot be used for compression.
+     *
+     * @test_category packaging, connection:compression
+     * @expected_result An exception is thrown at the time the cluster is built.
+     */
+    @Test(groups="unit", expectedExceptions=IllegalStateException.class,
+            expectedExceptionsMessageRegExp = "The requested compression is not available.*")
+    public void should_not_be_able_to_use_snappy() {
+        configure(Cluster.builder())
+                .addContactPointsWithPorts(Collections.singletonList(hostAddress)).
+                withCompression(ProtocolOptions.Compression.SNAPPY).build();
+    }
+
+    /**
+     * <p>
+     * Ensures that by default NIO is used for I/O for the underlying netty instance used by the driver.
+     *
+     * @test_category packaging
+     * @expected_result NioEventLoopGroup is used as the EventLoopGroup instance, NioSocketChannel is used as the
+     *                  Channel instance.
+     * @jira_ticket JAVA-676
+     * @since 2.0.10, 2.1.6
+     */
+    @Test(groups="unit")
+    public void should_use_nio_for_netty() {
+        NettyOptions nettyOptions = new NettyOptions();
+        assertThat(nettyOptions.channelClass()).isEqualTo(NioSocketChannel.class);
+        EventLoopGroup eventLoopGroup = nettyOptions.eventLoopGroup(new DefaultThreadFactory("test"));
+        try {
+            assertThat(eventLoopGroup).isInstanceOf(NioEventLoopGroup.class);
+        } finally {
+            eventLoopGroup.shutdownGracefully(0, 5, TimeUnit.SECONDS);
+        }
+    }
+}
