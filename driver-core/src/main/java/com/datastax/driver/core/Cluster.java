b322c4d9a271b000fa4dfc607a88861cd30257e5
Andrew Tolbert <andrew.tolbert@datastax.com>
2015-04-28 14:40:15 +0200

    Suppress Unexpected error in onFailure if cancelled (JAVA-710).

--- driver-core/src/main/java/com/datastax/driver/core/Cluster.java
+++ driver-core/src/main/java/com/datastax/driver/core/Cluster.java
@@ -1518,7 +1518,7 @@ public class Cluster implements Closeable {
                     } catch (ExecutionException e) {
                         Throwable t = e.getCause();
                         // That future is not really supposed to throw unexpected exceptions
-                        if (!(t instanceof InterruptedException))
+                        if (!(t instanceof InterruptedException) && !(t instanceof CancellationException))
                             logger.error("Unexpected error while marking node UP: while this shouldn't happen, this shouldn't be critical", t);
                     }
 
@@ -1789,7 +1789,7 @@ public class Cluster implements Closeable {
                     } catch (ExecutionException e) {
                         Throwable t = e.getCause();
                         // That future is not really supposed to throw unexpected exceptions
-                        if (!(t instanceof InterruptedException))
+                        if (!(t instanceof InterruptedException) && !(t instanceof CancellationException))
                             logger.error("Unexpected error while adding node: while this shouldn't happen, this shouldn't be critical", t);
                     }
 
