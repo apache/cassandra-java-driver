5f8ef9065e0f0eafd9a5341a0a834bd1b60390dc
Alexandre Dutra <alexandre.dutra@datastax.com>
2015-07-16 14:29:28 +0200

    Amend TupleTest to fix a failing test involving nested tuples.
    With C* 2.2 + (and JDK 8 consequently), a type containing 128 nested tuples causes the server to crash with a StackOverflowError. Lowering the number of nested tuples to 64 solves the problem.

--- driver-core/src/test/java/com/datastax/driver/core/TupleTest.java
+++ driver-core/src/test/java/com/datastax/driver/core/TupleTest.java
@@ -434,12 +434,12 @@ public class TupleTest extends CCMBridge.PerClassSingleNodeCluster {
                             "v_1 %s, " +
                             "v_2 %s, " +
                             "v_3 %s, " +
-                            "v_128 %s)", nestedTuplesSchemaHelper(1),
+                            "v_64 %s)", nestedTuplesSchemaHelper(1),
                                          nestedTuplesSchemaHelper(2),
                                          nestedTuplesSchemaHelper(3),
-                                         nestedTuplesSchemaHelper(128)));
+                                         nestedTuplesSchemaHelper(64)));
 
-            for (int i : Arrays.asList(1, 2, 3, 128)) {
+            for (int i : Arrays.asList(1, 2, 3, 64)) {
                 // create tuple
                 TupleValue createdTuple = nestedTuplesCreatorHelper(i);
 
