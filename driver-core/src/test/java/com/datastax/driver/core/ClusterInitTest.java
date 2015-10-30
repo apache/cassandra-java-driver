71d864216d7895a68decf7915dfa55ea72be3c85
Andrew Tolbert <andrew.tolbert@datastax.com>
2015-10-30 16:07:17 -0500

    Fix bug in test where only 1 node was used as a contact point.

--- driver-core/src/test/java/com/datastax/driver/core/ClusterInitTest.java
+++ driver-core/src/test/java/com/datastax/driver/core/ClusterInitTest.java
@@ -158,7 +158,7 @@ public class ClusterInitTest {
         // Both contact points time out so we're sure we'll try both of them and init will never complete.
         List<FakeHost> hosts = Lists.newArrayList(
             new FakeHost(CCMBridge.ipOfNode(0), 9042, THROWING_CONNECT_TIMEOUTS),
-            new FakeHost(CCMBridge.ipOfNode(0), 9042, THROWING_CONNECT_TIMEOUTS));
+            new FakeHost(CCMBridge.ipOfNode(1), 9042, THROWING_CONNECT_TIMEOUTS));
         // Use a low reconnection interval and keep the default connect timeout (5 seconds). So if a reconnection was scheduled,
         // we would see a call to the reconnection policy.
         CountingReconnectionPolicy reconnectionPolicy = new CountingReconnectionPolicy(new ConstantReconnectionPolicy(100));
