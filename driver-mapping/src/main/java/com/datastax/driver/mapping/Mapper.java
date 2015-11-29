5809df7bd1f4ca1d5e285eaf776707ddcd345133
Faisal Hameed <faisal.hameed@devfactory.com>
2015-11-29 18:50:49 +0500

    Fixing squid:S1699 : Constructors should only call non-overridable methods

--- driver-mapping/src/main/java/com/datastax/driver/mapping/Mapper.java
+++ driver-mapping/src/main/java/com/datastax/driver/mapping/Mapper.java
@@ -99,7 +99,7 @@ public class Mapper<T> {
         this.defaultDeleteOptions = NO_OPTIONS;
     }
 
-    Session session() {
+    final Session session() {
         return manager.getSession();
     }
 
