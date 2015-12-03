mklew/java-driver

JAVA-863: Idempotent Prepared Statements

driver-core/src/main/java/com/datastax/driver/core/BoundStatement.java
@@ -74,6 +74,12 @@ public class BoundStatement extends Statement implements SettableData<BoundState
             this.enableTracing();
         if (statement.getRetryPolicy() != null)
             this.setRetryPolicy(statement.getRetryPolicy());
+        if (statement instanceof IdempotenceAwarePreparedStatement) {
+            IdempotenceAwarePreparedStatement idempotenceAwarePS = (IdempotenceAwarePreparedStatement) statement;
+            if (idempotenceAwarePS.isIdempotent() != null) {
+                this.setIdempotent(idempotenceAwarePS.isIdempotent());
+            }
+        }
     }
 
     /**

apache/cassandra-java-driver

driver-core/src/main/java/com/datastax/driver/core/BoundStatement.java
@@ -87,6 +87,9 @@ public class BoundStatement extends Statement implements SettableData<BoundState
             // propagate incoming payload as outgoing payload, if no outgoing payload has been explicitly set
             this.setOutgoingPayload(statement.getIncomingPayload());
         this.codecRegistry = statement.getCodecRegistry();
+        if (statement.isIdempotent() != null) {
+            this.setIdempotent(statement.isIdempotent());
+        }
     }
 
     /**