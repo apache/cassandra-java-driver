8955f58d0d35e03c9a09c63143ca12db91db2c8a
Andrew Tolbert <andrew.tolbert@datastax.com>
2015-12-18 17:51:51 -0600

    Add support for using DataStax Enterprise with CCMBridge.
    Use -Ddse property for indicating DSE use.
    
    If -Ddse=true is present, -Dcassandra.version will be interpreted as the
    DSE version.
    
    Added static method to CCMBridge for getCassandraVersion, which detects
    the cassandra version from cassandra.version, or if DSE attempts to
    derive the C* version.
    
    Add withDSEConfiguration to CCMBridge.Builder.
    
    Add @DseVersion annotation to restrict tests based on DSE version
    requirement.  If DSE is not in use the test is simply skipped.
    Unlike @CassandraVersion, providing a major version is optional.
    
    Add support for multiple jvmArg on start.
    
    Move CCM log statement to CCMBridge.
    
    Update cassandra.branch parsing to trim branch and also replace quotes
    like 3.0 does.

--- driver-core/src/test/java/com/datastax/driver/core/utils/DseVersion.java
+++ driver-core/src/test/java/com/datastax/driver/core/utils/DseVersion.java
@@ -19,20 +19,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * <p>Annotation for a Class or Method that defines a Cassandra Version requirement.  If the cassandra version in use
- * does not meet the version requirement, the test is skipped.</p>
+ * <p>Annotation for a Class or Method that defines a DataStax Enterprise Version requirement.
+ * If the version in use does not meet the version requirement or DSE is not used, the test is skipped.</p>
  *
  * @see {@link com.datastax.driver.core.TestListener#beforeInvocation(org.testng.IInvokedMethod, org.testng.ITestResult)} for usage.
  */
 @Retention(RetentionPolicy.RUNTIME)
-public @interface CassandraVersion {
+public @interface DseVersion {
     /**
-     * @return The major version required to execute this test, i.e. "2.0"
+     * @return The major version required to execute this test, i.e. "4.8"
      */
-    double major();
+    double major() default 0.0;
 
     /**
-     * @return The minor version required to execute this test, i.e. "0"
+     * @return The minor version required to execute this test, i.e. "3"
      */
     int minor() default 0;
 
