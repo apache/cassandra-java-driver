37c39c72a1e0ad9e19558215304e469ca28dab05
Andrew Tolbert <andrew.tolbert@datastax.com>
2016-06-28 16:31:44 -0500

    Update tests to account for errors now including full socket address.

--- driver-core/src/test/java/com/datastax/driver/core/policies/FallthroughRetryPolicyIntegrationTest.java
+++ driver-core/src/test/java/com/datastax/driver/core/policies/FallthroughRetryPolicyIntegrationTest.java
@@ -96,7 +96,7 @@ public class FallthroughRetryPolicyIntegrationTest extends AbstractRetryPolicyIn
                 Fail.fail("expected an OperationTimedOutException");
             } catch (OperationTimedOutException e) {
                 assertThat(e.getMessage()).isEqualTo(
-                        String.format("[%s] Timed out waiting for server response", host1.getAddress())
+                        String.format("[%s] Timed out waiting for server response", host1.getSocketAddress())
                 );
             }
             assertOnRequestErrorWasCalled(1, OperationTimedOutException.class);
@@ -139,7 +139,7 @@ public class FallthroughRetryPolicyIntegrationTest extends AbstractRetryPolicyIn
             Fail.fail("expected a TransportException");
         } catch (TransportException e) {
             assertThat(e.getMessage()).isEqualTo(
-                    String.format("[%s] Connection has been closed", host1.getAddress())
+                    String.format("[%s] Connection has been closed", host1.getSocketAddress())
             );
         }
         assertOnRequestErrorWasCalled(1, TransportException.class);
