e38cbec6650279eb572cc9caf93605c71a2ce674
olim7t <omichallat+github@gmail.com>
2016-07-22 00:35:56 -0700

    Add RollingCount

---
+++ driver-core/src/main/java/com/datastax/driver/core/policies/RollingCount.java
@@ -0,0 +1,118 @@
+/*
+ *      Copyright (C) 2012-2015 DataStax Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.datastax.driver.core.policies;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicLongArray;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * A "rolling" count over a 1-minute sliding window.
+ */
+class RollingCount {
+    // Divide the minute into 5-second intervals
+    private static final long INTERVAL_SIZE = TimeUnit.SECONDS.toNanos(5);
+
+    // A circular buffer containing the counts over the 12 previous intervals. Their sum is the count we're looking for.
+    // If we're at t = 61s this would span [0,60[
+    private final AtomicLongArray previousIntervals = new AtomicLongArray(12);
+    // The interval we're currently recording events for. It hasn't completed yet, so it's not included in the count.
+    // If we're at t = 61s this would span [60,65[
+    // Note that we don't expect very high concurrency on RollingCount (it's used to count errors), so AtomicLong is
+    // good enough here.
+    private final AtomicLong currentInterval = new AtomicLong();
+    // Other mutable state, grouped in an object for atomic updates
+    private final AtomicReference<State> state;
+    private final Clock clock;
+
+    RollingCount(Clock clock) {
+        this.state = new AtomicReference<State>(new State(clock.nanoTime()));
+        this.clock = clock;
+    }
+
+    void increment() {
+        add(1);
+    }
+
+    void add(long amount) {
+        tickIfNecessary();
+        currentInterval.addAndGet(amount);
+    }
+
+    long get() {
+        tickIfNecessary();
+        return state.get().totalCount;
+    }
+
+    private void tickIfNecessary() {
+        State oldState = state.get();
+        long newTick = clock.nanoTime();
+        long age = newTick - oldState.lastTick;
+        if (age >= INTERVAL_SIZE) {
+            long currentCount = currentInterval.get();
+
+            long newIntervalStartTick = newTick - age % INTERVAL_SIZE;
+            long elapsedIntervals = Math.min(age / INTERVAL_SIZE, 12);
+            int newOffset = (int) ((oldState.offset + elapsedIntervals) % 12);
+
+            long newTotal;
+            if (elapsedIntervals == 12) {
+                // We wrapped around the circular buffer, all our values are stale
+                // Don't mutate previousIntervals yet because this part of the code is still multi-threaded.
+                newTotal = 0;
+            } else {
+                // Add the current interval that just completed
+                newTotal = oldState.totalCount + currentCount;
+                // Subtract all elapsed intervals: they're either idle ones, or the one at the old offset that we're
+                // about to replace
+                for (int i = 1; i <= elapsedIntervals; i++) {
+                    newTotal -= previousIntervals.get((newOffset + 12 - i) % 12);
+                }
+            }
+
+            State newState = new State(newIntervalStartTick, newOffset, newTotal);
+            if (state.compareAndSet(oldState, newState)) {
+                // Only one thread gets here, so we can now:
+                // - reset the current count (don't use reset because other threads might already have started updating
+                // it)
+                currentInterval.addAndGet(-currentCount);
+                // - store the interval that just completed (or clear it if we wrapped)
+                previousIntervals.set(oldState.offset, elapsedIntervals < 12 ? currentCount : 0);
+                // - clear any idle interval
+                for (int i = 1; i < elapsedIntervals; i++) {
+                    previousIntervals.set((newOffset + 12 - i) % 12, 0);
+                }
+            }
+        }
+    }
+
+    static class State {
+        final long lastTick; // last time the state was modified
+        final int offset; // the offset that the current interval will replace once it's complete
+        final long totalCount; // cache of sum(previousIntervals)
+
+        State(long lastTick) {
+            this(lastTick, 0, 0);
+        }
+
+        State(long lastTick, int offset, long totalCount) {
+            this.lastTick = lastTick;
+            this.offset = offset;
+            this.totalCount = totalCount;
+        }
+    }
+}
---
+++ driver-core/src/test/java/com/datastax/driver/core/policies/RollingCountTest.java
@@ -0,0 +1,149 @@
+/*
+ *      Copyright (C) 2012-2015 DataStax Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.datastax.driver.core.policies;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.datastax.driver.core.Assertions.assertThat;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.mockito.Mockito.when;
+
+public class RollingCountTest {
+    @Mock
+    Clock clock;
+
+    RollingCount rollingCount;
+
+    @BeforeMethod(groups = "unit")
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        assertThat(clock.nanoTime()).isEqualTo(0);
+        rollingCount = new RollingCount(clock);
+    }
+
+    @Test(groups = "unit")
+    public void should_record_adds_in_first_interval() {
+        // t = 0
+        rollingCount.increment();
+        setTime(1, SECONDS);
+        rollingCount.increment();
+        setTime(2, SECONDS);
+        rollingCount.increment();
+
+        // the count does not update in real time...
+        assertThat(rollingCount.get()).isEqualTo(0);
+
+        // but only at the end of each 5-second interval
+        setTime(5, SECONDS);
+        assertThat(rollingCount.get()).isEqualTo(3);
+    }
+
+    @Test(groups = "unit")
+    public void should_record_adds_over_two_intervals() {
+        rollingCount.add(2);
+
+        setTime(5, SECONDS); // 2nd interval
+        rollingCount.add(3);
+
+        setTime(10, SECONDS);
+        assertThat(rollingCount.get()).isEqualTo(5);
+    }
+
+    @Test(groups = "unit")
+    public void should_record_adds_separated_by_idle_intervals() {
+        rollingCount.add(2);
+
+        setTime(10, SECONDS); // 3rd interval
+        rollingCount.add(3);
+
+        setTime(15, SECONDS);
+        assertThat(rollingCount.get()).isEqualTo(5);
+    }
+
+    @Test(groups = "unit")
+    public void should_rotate() {
+        // 2 in [0,5[, then 1 every 5 seconds in [5,60[
+        rollingCount.add(2);
+        for (int i = 1; i < 12; i++) {
+            setTime(i * 5, SECONDS);
+            rollingCount.add(1);
+        }
+        setTime(60, SECONDS);
+        // the previous minute is now [0,60[
+        assertThat(rollingCount.get()).isEqualTo(13);
+
+        rollingCount.add(1);
+        setTime(65, SECONDS);
+        // the previous minute is now [5,65[, so the 2 events from [0,5[ should be forgotten
+        assertThat(rollingCount.get()).isEqualTo(12);
+    }
+
+    @Test(groups = "unit")
+    public void should_rotate_with_idle_intervals() {
+        // 1 every 5 seconds in [0,60[
+        for (int i = 0; i < 12; i++) {
+            setTime(i * 5, SECONDS);
+            rollingCount.add(1);
+        }
+        // idle in [60, 75[, then 1 in [75,80[
+        setTime(75, SECONDS);
+        rollingCount.add(1);
+
+        setTime(80, SECONDS);
+        // the last minute is [20,80[, with 1 every 5 seconds except during 15 seconds
+        assertThat(rollingCount.get()).isEqualTo(9);
+    }
+
+    @Test(groups = "unit")
+    public void should_rotate_when_idle_for_full_period() {
+        // 1 every 5 seconds in [0,60[
+        for (int i = 0; i < 12; i++) {
+            setTime(i * 5, SECONDS);
+            rollingCount.add(1);
+        }
+        // idle for the next minute [60,120[, then 1 in [120,125[
+        setTime(120, SECONDS);
+        rollingCount.add(1);
+
+        setTime(125, SECONDS);
+        assertThat(rollingCount.get()).isEqualTo(1);
+    }
+
+    @Test(groups = "unit")
+    public void should_rotate_when_idle_for_more_than_full_period() {
+        // 1 every 5 seconds in [0,60[
+        for (int i = 0; i < 12; i++) {
+            setTime(i * 5, SECONDS);
+            rollingCount.add(1);
+        }
+        // idle for the next minute and 5 seconds [60,125[, then 1 in [125,130[
+        setTime(125, SECONDS);
+        rollingCount.add(1);
+
+        setTime(130, SECONDS);
+        assertThat(rollingCount.get()).isEqualTo(1);
+    }
+
+    private void setTime(long time, TimeUnit unit) {
+        when(clock.nanoTime()).thenReturn(NANOSECONDS.convert(time, unit));
+    }
+}
