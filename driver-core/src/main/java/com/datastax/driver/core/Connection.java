40a908d55958f1a6fab83eddc087948d1d0e1926
Tim Lamballais Tessensohn <timothy.lamballais@booking.com>
2017-01-13 16:51:07 +0100

    Enable creation of SSLHandler with remote address
    This is needed to instantiate SSLEngine/SSLHandler with remote address
    information set, which is enables doing hostname verification (for example
    through a custom TrustManager). We cannot take this information from
    SocketChannel since it is not connected yet at the time Connection.Initializer
    creates the SSLHandler.

--- driver-core/src/main/java/com/datastax/driver/core/Connection.java
+++ driver-core/src/main/java/com/datastax/driver/core/Connection.java
@@ -1311,7 +1311,11 @@ class Connection {
             ChannelPipeline pipeline = channel.pipeline();
 
             if (sslOptions != null) {
-                pipeline.addLast("ssl", sslOptions.newSSLHandler(channel));
+                if (sslOptions instanceof AddressAwareSSLOptions) {
+                    pipeline.addLast("ssl", ((AddressAwareSSLOptions)sslOptions).newSSLHandler(channel, connection.address));
+                } else {
+                    pipeline.addLast("ssl", sslOptions.newSSLHandler(channel));
+                }
             }
 
 //            pipeline.addLast("debug", new LoggingHandler(LogLevel.INFO));
