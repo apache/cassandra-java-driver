2b196a94e7c664db22910856cc4e99a9d9156303
Andrew Tolbert <andrew.tolbert@datastax.com>
2017-08-10 12:49:02 -0500

    Version restrict MapperSaveNullFieldsTest
    Since test depends on timestamp behavior which is inconsistent with 2.0
    (may reuse same timestamp), require 2.1.

--- driver-mapping/src/test/java/com/datastax/driver/mapping/MapperSaveNullFieldsTest.java
+++ driver-mapping/src/test/java/com/datastax/driver/mapping/MapperSaveNullFieldsTest.java
@@ -17,7 +17,6 @@ package com.datastax.driver.mapping;
 
 import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.CCMTestsSupport;
-import com.datastax.driver.core.ProtocolVersion;
 import com.datastax.driver.core.utils.CassandraVersion;
 import com.datastax.driver.mapping.Mapper.Option;
 import com.datastax.driver.mapping.annotations.PartitionKey;
@@ -28,6 +27,7 @@ import org.testng.annotations.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("unused")
+@CassandraVersion("2.1.0")
 public class MapperSaveNullFieldsTest extends CCMTestsSupport {
 
     Mapper<User> mapper;
@@ -42,7 +42,6 @@ public class MapperSaveNullFieldsTest extends CCMTestsSupport {
         mapper = new MappingManager(session()).mapper(User.class);
     }
 
-    @CassandraVersion("2.1.0")
     @Test(groups = "short")
     void should_save_null_fields_if_requested() {
         should_save_null_fields(true, Option.saveNullFields(true));
