40e71ef7eab55a4828af51684a7aec17e07f70cd
olim7t <omichallat+github@gmail.com>
2017-09-30 21:14:30 -0700

    Fix bug when native library is not in the classpath
    We still want Native to initialize correctly, so use an inner class
    (like in 3.x).

--- core/src/main/java/com/datastax/oss/driver/internal/core/os/Native.java
+++ core/src/main/java/com/datastax/oss/driver/internal/core/os/Native.java
@@ -29,65 +29,79 @@ public class Native {
 
   private static final Logger LOG = LoggerFactory.getLogger(Native.class);
 
-  /** Handles libc calls through JNR (must be public). */
-  public interface LibC {
-    int gettimeofday(@Out @Transient Timeval tv, Pointer unused);
-  }
-
-  // See http://man7.org/linux/man-pages/man2/settimeofday.2.html
-  private static class Timeval extends Struct {
-    private final time_t tv_sec = new time_t();
-    private final Unsigned32 tv_usec = new Unsigned32();
-
-    private Timeval(Runtime runtime) {
-      super(runtime);
-    }
-  }
-
-  private static final LibC LIB_C;
-  private static final Runtime LIB_C_RUNTIME;
-
   /** Whether {@link Native#currentTimeMicros()} is available on this system. */
-  public static final boolean CURRENT_TIME_MICROS_AVAILABLE;
-
-  static {
-    LibC libc;
-    Runtime runtime = null;
+  public static boolean isCurrentTimeMicrosAvailable() {
     try {
-      libc = LibraryLoader.create(LibC.class).load("c");
-      runtime = Runtime.getRuntime(libc);
-    } catch (Throwable t) {
-      libc = null;
-      LOG.debug("Error loading libc", t);
+      return LibCLoader.GET_TIME_OF_DAY_AVAILABLE;
+    } catch (NoClassDefFoundError e) {
+      return false;
     }
-    LIB_C = libc;
-    LIB_C_RUNTIME = runtime;
-    boolean gettimeofday = false;
-    if (LIB_C_RUNTIME != null) {
-      try {
-        gettimeofday = LIB_C.gettimeofday(new Timeval(LIB_C_RUNTIME), null) == 0;
-      } catch (Throwable t) {
-        LOG.debug("Error accessing libc.gettimeofday()", t);
-      }
-    }
-    CURRENT_TIME_MICROS_AVAILABLE = gettimeofday;
   }
 
   /**
    * The current time in microseconds, as returned by libc.gettimeofday(); can only be used if
-   * {@link #CURRENT_TIME_MICROS_AVAILABLE} is true.
+   * {@link #isCurrentTimeMicrosAvailable()} is true.
    */
   public static long currentTimeMicros() {
-    if (!CURRENT_TIME_MICROS_AVAILABLE) {
+    if (!isCurrentTimeMicrosAvailable()) {
       throw new IllegalStateException(
           "Native call not available. "
-              + "Check CURRENT_TIME_MICROS_AVAILABLE before calling this method.");
+              + "Check isCurrentTimeMicrosAvailable() before calling this method.");
     }
-    Timeval tv = new Timeval(LIB_C_RUNTIME);
-    int res = LIB_C.gettimeofday(tv, null);
+    LibCLoader.Timeval tv = new LibCLoader.Timeval(LibCLoader.LIB_C_RUNTIME);
+    int res = LibCLoader.LIB_C.gettimeofday(tv, null);
     if (res != 0) {
       throw new IllegalStateException("Call to libc.gettimeofday() failed with result " + res);
     }
     return tv.tv_sec.get() * 1000000 + tv.tv_usec.get();
+  }
+
+  /**
+   * If jnr-ffi is not in the classpath at runtime, we'll fail to initialize the static fields
+   * below, but we still want {@link Native} to initialize successfully, so use an inner class.
+   */
+  private static class LibCLoader {
+
+    /** Handles libc calls through JNR (must be public). */
+    public interface LibC {
+      int gettimeofday(@Out @Transient Timeval tv, Pointer unused);
+    }
+
+    // See http://man7.org/linux/man-pages/man2/settimeofday.2.html
+    private static class Timeval extends Struct {
+      private final time_t tv_sec = new time_t();
+      private final Unsigned32 tv_usec = new Unsigned32();
+
+      private Timeval(Runtime runtime) {
+        super(runtime);
+      }
+    }
+
+    private static final LibC LIB_C;
+    private static final Runtime LIB_C_RUNTIME;
+    private static final boolean GET_TIME_OF_DAY_AVAILABLE;
+
+    static {
+      LibC libc;
+      Runtime runtime = null;
+      try {
+        libc = LibraryLoader.create(LibC.class).load("c");
+        runtime = Runtime.getRuntime(libc);
+      } catch (Throwable t) {
+        libc = null;
+        LOG.debug("Error loading libc", t);
+      }
+      LIB_C = libc;
+      LIB_C_RUNTIME = runtime;
+      boolean getTimeOfDayAvailable = false;
+      if (LIB_C_RUNTIME != null) {
+        try {
+          getTimeOfDayAvailable = LIB_C.gettimeofday(new Timeval(LIB_C_RUNTIME), null) == 0;
+        } catch (Throwable t) {
+          LOG.debug("Error accessing libc.gettimeofday()", t);
+        }
+      }
+      GET_TIME_OF_DAY_AVAILABLE = getTimeOfDayAvailable;
+    }
   }
 }
