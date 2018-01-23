44c4799b8cfb94a317b90098b2ced2a68bf8797f
GregBestland <Greg.Bestland@datastax.com>
2018-01-23 17:42:40 -0600

    JAVA-1727: Override DefaultUdtValue.equals

--- core/src/test/java/com/datastax/oss/driver/internal/core/data/DefaultUdtValueTest.java
+++ core/src/test/java/com/datastax/oss/driver/internal/core/data/DefaultUdtValueTest.java
@@ -63,4 +63,33 @@ public class DefaultUdtValueTest extends AccessibleByIdTestBase<UdtValue> {
     assertThat(Bytes.toHexString(out.getBytesUnsafe(0))).isEqualTo("0x00000001");
     assertThat(Bytes.toHexString(out.getBytesUnsafe(1))).isEqualTo("0x61");
   }
+
+  @Test
+  public void should_equate_instances_with_same_values_but_different_binary_representations() {
+    UserDefinedType type =
+        new UserDefinedTypeBuilder(
+                CqlIdentifier.fromInternal("ks"), CqlIdentifier.fromInternal("type"))
+            .withField(CqlIdentifier.fromInternal("f"), DataTypes.VARINT)
+            .build();
+
+    UdtValue udt1 = type.newValue().setBytesUnsafe(0, Bytes.fromHexString("0x01"));
+    UdtValue udt2 = type.newValue().setBytesUnsafe(0, Bytes.fromHexString("0x0001"));
+
+    assertThat(udt1).isEqualTo(udt2);
+  }
+
+  @Test
+  public void should_format_to_string() {
+    UserDefinedType type =
+        new UserDefinedTypeBuilder(
+                CqlIdentifier.fromInternal("ks"), CqlIdentifier.fromInternal("type"))
+            .withField(CqlIdentifier.fromInternal("t"), DataTypes.TEXT)
+            .withField(CqlIdentifier.fromInternal("i"), DataTypes.INT)
+            .withField(CqlIdentifier.fromInternal("d"), DataTypes.DOUBLE)
+            .build();
+
+    UdtValue udt = type.newValue().setString("t", "foobar").setDouble("d", 3.14);
+
+    assertThat(udt.toString()).isEqualTo("{t:'foobar',i:NULL,d:3.14}");
+  }
 }
