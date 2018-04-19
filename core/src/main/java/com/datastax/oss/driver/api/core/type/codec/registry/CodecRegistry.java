1842a32213539dce735fedaa5ec890e93be9a5ca
olim7t <omichallat+github@gmail.com>
2018-04-19 14:36:52 -0700

    Improve CodecRegistry javadocs

--- core/src/main/java/com/datastax/oss/driver/api/core/type/codec/registry/CodecRegistry.java
+++ core/src/main/java/com/datastax/oss/driver/api/core/type/codec/registry/CodecRegistry.java
@@ -16,6 +16,8 @@
 package com.datastax.oss.driver.api.core.type.codec.registry;
 
 import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.core.data.GettableByIndex;
 import com.datastax.oss.driver.api.core.data.TupleValue;
 import com.datastax.oss.driver.api.core.type.DataType;
 import com.datastax.oss.driver.api.core.type.DataTypes;
@@ -23,6 +25,7 @@ import com.datastax.oss.driver.api.core.type.codec.CodecNotFoundException;
 import com.datastax.oss.driver.api.core.type.codec.TypeCodec;
 import com.datastax.oss.driver.api.core.type.reflect.GenericType;
 import com.datastax.oss.driver.internal.core.type.codec.registry.DefaultCodecRegistry;
+import java.util.List;
 
 /**
  * Provides codecs to convert CQL types to their Java equivalent, and vice-versa.
@@ -44,16 +47,25 @@ public interface CodecRegistry {
   /**
    * Returns a codec to handle the conversion between the given types.
    *
+   * <p>This is used internally by the driver, in cases where both types are known, for example
+   * {@link GettableByIndex#getString(int) row.getString(0)} (Java type inferred from the method,
+   * CQL type known from the row metadata).
+   *
+   * <p>The driver's default registry implementation is <em>invariant</em> with regard to the Java
+   * type: for example, if {@code B extends A} and an {@code A<=>int} codec is registered, {@code
+   * codecFor(DataTypes.INT, B.class)} <b>will not</b> find that codec. This is because this method
+   * is used internally both for encoding and decoding, and covariance wouldn't work when decoding.
+   *
    * @throws CodecNotFoundException if there is no such codec.
    */
   <T> TypeCodec<T> codecFor(DataType cqlType, GenericType<T> javaType);
 
   /**
-   * Returns a codec to handle the conversion between the given types.
+   * Shortcut for {@link #codecFor(DataType, GenericType) codecFor(cqlType,
+   * GenericType.of(javaType))}.
    *
-   * <p>The default implementation of this method simply wraps {@code javaType} and calls {@link
-   * #codecFor(DataType, GenericType)}. Implementors can override it if they can avoid the overhead
-   * of wrapping.
+   * <p>Implementations may decide to override this method for performance reasons, if they have a
+   * way to avoid the overhead of wrapping.
    *
    * @throws CodecNotFoundException if there is no such codec.
    */
@@ -65,8 +77,14 @@ public interface CodecRegistry {
    * Returns a codec to convert the given CQL type to the Java type deemed most appropriate to
    * represent it.
    *
-   * <p>The definition of "most appropriate" is unspecified, and left to the appreciation of the
-   * registry implementor.
+   * <p>This is used internally by the driver, in cases where the Java type is not explicitly
+   * provided, for example {@link GettableByIndex#getObject(int) row.getObject(0)} (CQL type known
+   * from the row metadata, Java type unspecified).
+   *
+   * <p>The definition of "most appropriate" is left to the appreciation of the registry
+   * implementor.
+   *
+   * @throws CodecNotFoundException if there is no such codec.
    */
   <T> TypeCodec<T> codecFor(DataType cqlType);
 
@@ -74,17 +92,25 @@ public interface CodecRegistry {
    * Returns a codec to convert the given Java type to the CQL type deemed most appropriate to
    * represent it.
    *
-   * <p>The definition of "most appropriate" is unspecified, and left to the appreciation of the
-   * registry implementor.
+   * <p>The driver does not use this method. It is provided as a convenience for third-party usage,
+   * for example if you were to generate a schema based on a set of Java classes.
+   *
+   * <p>The driver's default registry implementation is <em>invariant</em> with regard to the Java
+   * type: for example, if {@code B extends A} and an {@code A<=>int} codec is registered, {@code
+   * codecFor(DataTypes.INT, B.class)} <b>will not</b> find that codec. This is because we don't
+   * know whether this method will be used for encoding, decoding, or both.
+   *
+   * @throws CodecNotFoundException if there is no such codec.
    */
   <T> TypeCodec<T> codecFor(GenericType<T> javaType);
 
   /**
-   * Returns a codec to convert the given Java type to the CQL type deemed most appropriate to
-   * represent it.
+   * Shortcut for {@link #codecFor(GenericType) codecFor(GenericType.of(javaType))}.
    *
-   * <p>The definition of "most appropriate" is unspecified, and left to the appreciation of the
-   * registry implementor.
+   * <p>Implementations may decide to override this method for performance reasons, if they have a
+   * way to avoid the overhead of wrapping.
+   *
+   * @throws CodecNotFoundException if there is no such codec.
    */
   default <T> TypeCodec<T> codecFor(Class<T> javaType) {
     return codecFor(GenericType.of(javaType));
@@ -94,8 +120,18 @@ public interface CodecRegistry {
    * Returns a codec to convert the given Java object to the CQL type deemed most appropriate to
    * represent it.
    *
-   * <p>The definition of "most appropriate" is unspecified, and left to the appreciation of the
-   * registry implementor.
+   * <p>This is used internally by the driver, in cases where the CQL type is unknown, for example
+   * for {@linkplain SimpleStatement#setPositionalValues(List) simple statement variables} (simple
+   * statements don't have access to schema metadata).
+   *
+   * <p>Unlike other methods, the driver's default registry implementation is <em>covariant</em>
+   * with regard to the Java type: for example, if {@code B extends A} and an {@code A<=>int} codec
+   * is registered, {@code codecFor(someB)} <b>will</b> find that codec. This is because this method
+   * is always used in encoding scenarios; if a simple statement has a value with a runtime type of
+   * {@code ArrayList<String>}, it should be possible to encode it with a codec that accepts a
+   * {@code List<String>}.
+   *
+   * @throws CodecNotFoundException if there is no such codec.
    */
   <T> TypeCodec<T> codecFor(T value);
 }
