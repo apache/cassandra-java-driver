6e1e5e71abfd0d5f36d9ad80ae069a6bbe4a9ec1
Alexandre Dutra <alexdut@gmail.com>
2018-08-16 18:32:05 +0200

    JAVA-1916: Base TimestampCodec.parse on java.util.Date

--- core/src/main/java/com/datastax/oss/driver/internal/core/type/codec/TimestampCodec.java
+++ core/src/main/java/com/datastax/oss/driver/internal/core/type/codec/TimestampCodec.java
@@ -15,8 +15,6 @@
  */
 package com.datastax.oss.driver.internal.core.type.codec;
 
-import static java.lang.Long.parseLong;
-
 import com.datastax.oss.driver.api.core.ProtocolVersion;
 import com.datastax.oss.driver.api.core.type.DataType;
 import com.datastax.oss.driver.api.core.type.DataTypes;
@@ -26,44 +24,186 @@ import com.datastax.oss.driver.api.core.type.reflect.GenericType;
 import com.datastax.oss.driver.internal.core.util.Strings;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
+import io.netty.util.concurrent.FastThreadLocal;
 import java.nio.ByteBuffer;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.temporal.ChronoField;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.TimeZone;
 import net.jcip.annotations.ThreadSafe;
 
+/**
+ * A codec that handles Apache Cassandra(R)'s timestamp type and maps it to Java's {@link Instant}.
+ *
+ * <p>Implementation notes:
+ *
+ * <ol>
+ *   <li>Because {@code Instant} uses a precision of nanoseconds, whereas the timestamp type uses a
+ *       precision of milliseconds, truncation will happen for any excess precision information as
+ *       though the amount in nanoseconds was subject to integer division by one million.
+ *   <li>For compatibility reasons, this codec uses the legacy {@link SimpleDateFormat} API
+ *       internally when parsing and formatting, and converts from {@link Instant} to {@link Date}
+ *       and vice versa. Specially when parsing, this may yield different results as compared to
+ *       what the newer Java Time API parsers would have produced for the same input.
+ *   <li>Also, {@code Instant} can store points on the time-line further in the future and further
+ *       in the past than {@code Date}. This codec will throw an exception when attempting to parse
+ *       or format an {@code Instant} falling in this category.
+ * </ol>
+ *
+ * <h3>Accepted date-time formats</h3>
+ *
+ * The following patterns are valid CQL timestamp literal formats for Apache Cassandra(R) 3.0 and
+ * higher, and are thus all recognized when parsing:
+ *
+ * <ol>
+ *   <li>{@code yyyy-MM-dd'T'HH:mm}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ss}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ss.SSS}
+ *   <li>{@code yyyy-MM-dd'T'HH:mmX}
+ *   <li>{@code yyyy-MM-dd'T'HH:mmXX}
+ *   <li>{@code yyyy-MM-dd'T'HH:mmXXX}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ssX}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ssXX}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ssXXX}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ss.SSSX}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ss.SSSXX}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ss.SSSXXX}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm z}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ss z}
+ *   <li>{@code yyyy-MM-dd'T'HH:mm:ss.SSS z}
+ *   <li>{@code yyyy-MM-dd HH:mm}
+ *   <li>{@code yyyy-MM-dd HH:mm:ss}
+ *   <li>{@code yyyy-MM-dd HH:mm:ss.SSS}
+ *   <li>{@code yyyy-MM-dd HH:mmX}
+ *   <li>{@code yyyy-MM-dd HH:mmXX}
+ *   <li>{@code yyyy-MM-dd HH:mmXXX}
+ *   <li>{@code yyyy-MM-dd HH:mm:ssX}
+ *   <li>{@code yyyy-MM-dd HH:mm:ssXX}
+ *   <li>{@code yyyy-MM-dd HH:mm:ssXXX}
+ *   <li>{@code yyyy-MM-dd HH:mm:ss.SSSX}
+ *   <li>{@code yyyy-MM-dd HH:mm:ss.SSSXX}
+ *   <li>{@code yyyy-MM-dd HH:mm:ss.SSSXXX}
+ *   <li>{@code yyyy-MM-dd HH:mm z}
+ *   <li>{@code yyyy-MM-dd HH:mm:ss z}
+ *   <li>{@code yyyy-MM-dd HH:mm:ss.SSS z}
+ *   <li>{@code yyyy-MM-dd}
+ *   <li>{@code yyyy-MM-ddX}
+ *   <li>{@code yyyy-MM-ddXX}
+ *   <li>{@code yyyy-MM-ddXXX}
+ *   <li>{@code yyyy-MM-dd z}
+ * </ol>
+ *
+ * By default, when parsing, timestamp literals that do not include any time zone information will
+ * be interpreted using the system's {@linkplain TimeZone#getDefault() default time zone}. This is
+ * intended to mimic Apache Cassandra(R)'s own parsing behavior (see {@code
+ * org.apache.cassandra.serializers.TimestampSerializer}). The default time zone can be modified
+ * using the {@linkplain TimestampCodec#TimestampCodec(ZoneId) one-arg constructor} that takes a
+ * custom {@link ZoneId} as an argument.
+ *
+ * <p>When formatting, the pattern used is always {@code yyyy-MM-dd'T'HH:mm:ss.SSSXXX} and the time
+ * zone is either the the system's default one, or the one that was provided when instantiating the
+ * codec.
+ */
 @ThreadSafe
 public class TimestampCodec implements TypeCodec<Instant> {
 
-  /** A {@link DateTimeFormatter} that parses (most) of the ISO formats accepted in CQL. */
-  private static final DateTimeFormatter PARSER =
-      new java.time.format.DateTimeFormatterBuilder()
-          .parseCaseSensitive()
-          .parseStrict()
-          .append(DateTimeFormatter.ISO_LOCAL_DATE)
-          .optionalStart()
-          .appendLiteral('T')
-          .appendValue(ChronoField.HOUR_OF_DAY, 2)
-          .appendLiteral(':')
-          .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
-          .optionalEnd()
-          .optionalStart()
-          .appendLiteral(':')
-          .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
-          .optionalEnd()
-          .optionalStart()
-          .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
-          .optionalEnd()
-          .optionalStart()
-          .appendZoneId()
-          .optionalEnd()
-          .toFormatter()
-          .withZone(ZoneOffset.UTC);
+  /**
+   * Patterns accepted by Apache Cassandra(R) 3.0 and higher when parsing CQL literals.
+   *
+   * <p>Note that Cassandra's TimestampSerializer declares many more patterns but some of them are
+   * equivalent when parsing.
+   */
+  private static final String[] DATE_STRING_PATTERNS =
+      new String[] {
+        // 1) date-time patterns separated by 'T'
+        // (declared first because none of the others are ISO compliant, but some of these are)
+        // 1.a) without time zone
+        "yyyy-MM-dd'T'HH:mm",
+        "yyyy-MM-dd'T'HH:mm:ss",
+        "yyyy-MM-dd'T'HH:mm:ss.SSS",
+        // 1.b) with ISO-8601 time zone
+        "yyyy-MM-dd'T'HH:mmX",
+        "yyyy-MM-dd'T'HH:mmXX",
+        "yyyy-MM-dd'T'HH:mmXXX",
+        "yyyy-MM-dd'T'HH:mm:ssX",
+        "yyyy-MM-dd'T'HH:mm:ssXX",
+        "yyyy-MM-dd'T'HH:mm:ssXXX",
+        "yyyy-MM-dd'T'HH:mm:ss.SSSX",
+        "yyyy-MM-dd'T'HH:mm:ss.SSSXX",
+        "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
+        // 1.c) with generic time zone
+        "yyyy-MM-dd'T'HH:mm z",
+        "yyyy-MM-dd'T'HH:mm:ss z",
+        "yyyy-MM-dd'T'HH:mm:ss.SSS z",
+        // 2) date-time patterns separated by whitespace
+        // 2.a) without time zone
+        "yyyy-MM-dd HH:mm",
+        "yyyy-MM-dd HH:mm:ss",
+        "yyyy-MM-dd HH:mm:ss.SSS",
+        // 2.b) with ISO-8601 time zone
+        "yyyy-MM-dd HH:mmX",
+        "yyyy-MM-dd HH:mmXX",
+        "yyyy-MM-dd HH:mmXXX",
+        "yyyy-MM-dd HH:mm:ssX",
+        "yyyy-MM-dd HH:mm:ssXX",
+        "yyyy-MM-dd HH:mm:ssXXX",
+        "yyyy-MM-dd HH:mm:ss.SSSX",
+        "yyyy-MM-dd HH:mm:ss.SSSXX",
+        "yyyy-MM-dd HH:mm:ss.SSSXXX",
+        // 2.c) with generic time zone
+        "yyyy-MM-dd HH:mm z",
+        "yyyy-MM-dd HH:mm:ss z",
+        "yyyy-MM-dd HH:mm:ss.SSS z",
+        // 3) date patterns without time
+        // 3.a) without time zone
+        "yyyy-MM-dd",
+        // 3.b) with ISO-8601 time zone
+        "yyyy-MM-ddX",
+        "yyyy-MM-ddXX",
+        "yyyy-MM-ddXXX",
+        // 3.c) with generic time zone
+        "yyyy-MM-dd z"
+      };
 
-  private static final DateTimeFormatter FORMATTER =
-      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSxxx").withZone(ZoneOffset.UTC);
+  private final FastThreadLocal<SimpleDateFormat> parser;
+
+  private final FastThreadLocal<SimpleDateFormat> formatter;
+
+  /**
+   * Creates a new {@code TimestampCodec} that uses the system's {@linkplain ZoneId#systemDefault()
+   * default time zone} to parse timestamp literals that do not include any time zone information.
+   */
+  public TimestampCodec() {
+    this(ZoneId.systemDefault());
+  }
+
+  /**
+   * Creates a new {@code TimestampCodec} that uses the given {@link ZoneId} to parse timestamp
+   * literals that do not include any time zone information.
+   */
+  public TimestampCodec(ZoneId defaultZoneId) {
+    parser =
+        new FastThreadLocal<SimpleDateFormat>() {
+          @Override
+          protected SimpleDateFormat initialValue() {
+            SimpleDateFormat parser = new SimpleDateFormat();
+            parser.setLenient(false);
+            parser.setTimeZone(TimeZone.getTimeZone(defaultZoneId));
+            return parser;
+          }
+        };
+    formatter =
+        new FastThreadLocal<SimpleDateFormat>() {
+          @Override
+          protected SimpleDateFormat initialValue() {
+            SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+            parser.setTimeZone(TimeZone.getTimeZone(defaultZoneId));
+            return parser;
+          }
+        };
+  }
 
   @NonNull
   @Override
@@ -106,7 +246,7 @@ public class TimestampCodec implements TypeCodec<Instant> {
   @NonNull
   @Override
   public String format(@Nullable Instant value) {
-    return (value == null) ? "NULL" : Strings.quote(FORMATTER.format(value));
+    return (value == null) ? "NULL" : Strings.quote(formatter.get().format(Date.from(value)));
   }
 
   @Nullable
@@ -115,21 +255,38 @@ public class TimestampCodec implements TypeCodec<Instant> {
     if (value == null || value.isEmpty() || value.equalsIgnoreCase("NULL")) {
       return null;
     }
-    // strip enclosing single quotes, if any
-    if (Strings.isQuoted(value)) {
-      value = Strings.unquote(value);
-    }
-    if (Strings.isLongLiteral(value)) {
+    String unquoted = Strings.unquote(value);
+    if (Strings.isLongLiteral(unquoted)) {
+      // Numeric literals may be quoted or not
       try {
-        return Instant.ofEpochMilli(parseLong(value));
+        return Instant.ofEpochMilli(Long.parseLong(unquoted));
       } catch (NumberFormatException e) {
         throw new IllegalArgumentException(
             String.format("Cannot parse timestamp value from \"%s\"", value));
       }
-    }
-    try {
-      return Instant.from(PARSER.parse(value));
-    } catch (DateTimeParseException e) {
+    } else {
+      // Alphanumeric literals must be quoted
+      if (!Strings.isQuoted(value)) {
+        throw new IllegalArgumentException(
+            String.format("Alphanumeric timestamp literal must be quoted: \"%s\"", value));
+      }
+      SimpleDateFormat parser = this.parser.get();
+      TimeZone timeZone = parser.getTimeZone();
+      ParsePosition pos = new ParsePosition(0);
+      for (String pattern : DATE_STRING_PATTERNS) {
+        parser.applyPattern(pattern);
+        pos.setIndex(0);
+        try {
+          Date date = parser.parse(unquoted, pos);
+          if (date != null && pos.getIndex() == unquoted.length()) {
+            return date.toInstant();
+          }
+        } finally {
+          // restore the parser's default time zone, it might have been modified by the call to
+          // parse()
+          parser.setTimeZone(timeZone);
+        }
+      }
       throw new IllegalArgumentException(
           String.format("Cannot parse timestamp value from \"%s\"", value));
     }
--- core/src/test/java/com/datastax/oss/driver/internal/core/type/codec/TimestampCodecTest.java
+++ core/src/test/java/com/datastax/oss/driver/internal/core/type/codec/TimestampCodecTest.java
@@ -15,16 +15,29 @@
  */
 package com.datastax.oss.driver.internal.core.type.codec;
 
+import static java.time.ZoneOffset.ofHours;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import com.datastax.oss.driver.api.core.type.codec.TypeCodecs;
+import com.datastax.oss.driver.shaded.guava.common.collect.Lists;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(DataProviderRunner.class)
 public class TimestampCodecTest extends CodecTestBase<Instant> {
 
   public TimestampCodecTest() {
-    this.codec = TypeCodecs.TIMESTAMP;
+    // force a given timezone for reproducible results in should_format
+    codec = new TimestampCodec(ZoneOffset.UTC);
   }
 
   @Test
@@ -53,28 +66,109 @@ public class TimestampCodecTest extends CodecTestBase<Instant> {
 
   @Test
   public void should_format() {
-    // No need to test various values because the codec delegates directly to the JDK's formatter,
+    // No need to test various values because the codec delegates directly to SimpleDateFormat,
     // which we assume does its job correctly.
-    assertThat(format(Instant.EPOCH)).isEqualTo("'1970-01-01T00:00:00.000+00:00'");
+    assertThat(format(Instant.EPOCH)).isEqualTo("'1970-01-01T00:00:00.000Z'");
+    assertThat(format(Instant.parse("2018-08-16T15:59:34.123Z")))
+        .isEqualTo("'2018-08-16T15:59:34.123Z'");
     assertThat(format(null)).isEqualTo("NULL");
   }
 
-  @Test
-  public void should_parse() {
-    // Raw number
-    assertThat(parse("'0'")).isEqualTo(Instant.EPOCH);
-
-    // Date format
-    assertThat(parse("'1970-01-01T00:00Z'")).isEqualTo(Instant.EPOCH);
-
-    assertThat(parse("NULL")).isNull();
-    assertThat(parse("null")).isNull();
-    assertThat(parse("")).isNull();
-    assertThat(parse(null)).isNull();
+  @DataProvider
+  public static Iterable<ZoneId> timeZones() {
+    return Lists.newArrayList(
+        ZoneId.systemDefault(),
+        ZoneOffset.UTC,
+        ZoneOffset.ofHoursMinutes(3, 30),
+        ZoneId.of("Europe/Paris"),
+        ZoneId.of("GMT+7"));
   }
 
-  @Test(expected = IllegalArgumentException.class)
+  @Test
+  @UseDataProvider("timeZones")
+  public void should_parse(ZoneId defaultTimeZone) {
+    TimestampCodec codec = new TimestampCodec(defaultTimeZone);
+
+    // Raw numbers
+    assertThat(codec.parse("'0'")).isEqualTo(Instant.EPOCH);
+    assertThat(codec.parse("'-1'")).isEqualTo(Instant.EPOCH.minusMillis(1));
+    assertThat(codec.parse("1534463100000")).isEqualTo(Instant.ofEpochMilli(1534463100000L));
+
+    // Date formats
+    Instant expected;
+
+    // date without time, without time zone
+    expected = LocalDate.parse("2017-01-01").atStartOfDay().atZone(defaultTimeZone).toInstant();
+    assertThat(codec.parse("'2017-01-01'")).isEqualTo(expected);
+
+    // date without time, with time zone
+    expected = LocalDate.parse("2018-08-16").atStartOfDay().atZone(ofHours(2)).toInstant();
+    assertThat(codec.parse("'2018-08-16+02'")).isEqualTo(expected);
+    assertThat(codec.parse("'2018-08-16+0200'")).isEqualTo(expected);
+    assertThat(codec.parse("'2018-08-16+02:00'")).isEqualTo(expected);
+    assertThat(codec.parse("'2018-08-16 CEST'")).isEqualTo(expected);
+
+    // date with time, without time zone
+    expected = LocalDateTime.parse("2018-08-16T23:45").atZone(defaultTimeZone).toInstant();
+    assertThat(codec.parse("'2018-08-16T23:45'")).isEqualTo(expected);
+    assertThat(codec.parse("'2018-08-16 23:45'")).isEqualTo(expected);
+
+    // date with time + seconds, without time zone
+    expected = LocalDateTime.parse("2019-12-31T16:08:38").atZone(defaultTimeZone).toInstant();
+    assertThat(codec.parse("'2019-12-31T16:08:38'")).isEqualTo(expected);
+    assertThat(codec.parse("'2019-12-31 16:08:38'")).isEqualTo(expected);
+
+    // date with time + seconds + milliseconds, without time zone
+    expected = LocalDateTime.parse("1950-02-28T12:00:59.230").atZone(defaultTimeZone).toInstant();
+    assertThat(codec.parse("'1950-02-28T12:00:59.230'")).isEqualTo(expected);
+    assertThat(codec.parse("'1950-02-28 12:00:59.230'")).isEqualTo(expected);
+
+    // date with time, with time zone
+    expected = ZonedDateTime.parse("1973-06-23T23:59:00.000+01:00").toInstant();
+    assertThat(codec.parse("'1973-06-23T23:59+01'")).isEqualTo(expected);
+    assertThat(codec.parse("'1973-06-23T23:59+0100'")).isEqualTo(expected);
+    assertThat(codec.parse("'1973-06-23T23:59+01:00'")).isEqualTo(expected);
+    assertThat(codec.parse("'1973-06-23T23:59 CET'")).isEqualTo(expected);
+    assertThat(codec.parse("'1973-06-23 23:59+01'")).isEqualTo(expected);
+    assertThat(codec.parse("'1973-06-23 23:59+0100'")).isEqualTo(expected);
+    assertThat(codec.parse("'1973-06-23 23:59+01:00'")).isEqualTo(expected);
+    assertThat(codec.parse("'1973-06-23 23:59 CET'")).isEqualTo(expected);
+
+    // date with time + seconds, with time zone
+    expected = ZonedDateTime.parse("1980-01-01T23:59:59.000-08:00").toInstant();
+    assertThat(codec.parse("'1980-01-01T23:59:59-08'")).isEqualTo(expected);
+    assertThat(codec.parse("'1980-01-01T23:59:59-0800'")).isEqualTo(expected);
+    assertThat(codec.parse("'1980-01-01T23:59:59-08:00'")).isEqualTo(expected);
+    assertThat(codec.parse("'1980-01-01T23:59:59 PST'")).isEqualTo(expected);
+    assertThat(codec.parse("'1980-01-01 23:59:59-08'")).isEqualTo(expected);
+    assertThat(codec.parse("'1980-01-01 23:59:59-0800'")).isEqualTo(expected);
+    assertThat(codec.parse("'1980-01-01 23:59:59-08:00'")).isEqualTo(expected);
+    assertThat(codec.parse("'1980-01-01 23:59:59 PST'")).isEqualTo(expected);
+
+    // date with time + seconds + milliseconds, with time zone
+    expected = ZonedDateTime.parse("1999-12-31T23:59:59.999+00:00").toInstant();
+    assertThat(codec.parse("'1999-12-31T23:59:59.999+00'")).isEqualTo(expected);
+    assertThat(codec.parse("'1999-12-31T23:59:59.999+0000'")).isEqualTo(expected);
+    assertThat(codec.parse("'1999-12-31T23:59:59.999+00:00'")).isEqualTo(expected);
+    assertThat(codec.parse("'1999-12-31T23:59:59.999 UTC'")).isEqualTo(expected);
+    assertThat(codec.parse("'1999-12-31 23:59:59.999+00'")).isEqualTo(expected);
+    assertThat(codec.parse("'1999-12-31 23:59:59.999+0000'")).isEqualTo(expected);
+    assertThat(codec.parse("'1999-12-31 23:59:59.999+00:00'")).isEqualTo(expected);
+    assertThat(codec.parse("'1999-12-31 23:59:59.999 UTC'")).isEqualTo(expected);
+
+    assertThat(codec.parse("NULL")).isNull();
+    assertThat(codec.parse("null")).isNull();
+    assertThat(codec.parse("")).isNull();
+    assertThat(codec.parse(null)).isNull();
+  }
+
+  @Test
   public void should_fail_to_parse_invalid_input() {
-    parse("not a timestamp");
+    assertThatThrownBy(() -> parse("not a timestamp"))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Alphanumeric timestamp literal must be quoted: \"not a timestamp\"");
+    assertThatThrownBy(() -> parse("'not a timestamp'"))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Cannot parse timestamp value from \"'not a timestamp'\"");
   }
 }
