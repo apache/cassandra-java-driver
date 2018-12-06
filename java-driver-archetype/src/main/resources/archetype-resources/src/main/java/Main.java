a547fad29b8136461bc55625b2e111f41c76f5ca
Erik Merkle <erik.merkle@datastax.com>
2018-12-06 11:27:12 -0600

    [fixup] implement an interactive cqlsh example

--- java-driver-archetype/src/main/resources/archetype-resources/src/main/java/Main.java
+++ java-driver-archetype/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -3,20 +3,81 @@ package ${package};
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.CqlSessionBuilder;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.PrintStream;
 import java.net.InetSocketAddress;
+import java.util.Iterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
+
   private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
+  private final PrintStream output;
+
+  private Main() {
+    this.output = System.out;
+  }
+
+  private void promptForQuery(String msg) {
+    if (msg != null) {
+      output.println();
+      output.println(msg);
+    }
+    output.println();
+    output.print("cql-demo> ");
+  }
+
+  /**
+   * Basic cqlsh-like prompt. It loops until the user types "EXIT", executing queries and dumping
+   * the response to the console (System.out).
+   */
+  private void cqlshLite(CqlSession session, LineNumberReader input) throws IOException {
+    // provide a prompt
+    promptForQuery("This is a CQL demo. Type 'EXIT' to quit.");
+    // get the query
+    String query = input.readLine();
+    ResultSet rs;
+    // execute the query
+    while (!"exit".equalsIgnoreCase(query)) {
+      try {
+        rs = session.execute(query);
+        Iterator<Row> iterator = rs.iterator();
+        while (iterator.hasNext()) {
+          Row row = iterator.next();
+          System.out.println("Response: '" + row.getObject(0).toString() + "'");
+        }
+      } catch (Exception ex) {
+        // something went wrong with the query, just dump the stacktrace to the output
+        ex.printStackTrace(output);
+      }
+      // provide another prompt
+      promptForQuery(null);
+      // get the next query
+      query = input.readLine();
+    }
+  }
+
   public static void main(String[] args) {
+    Main main = new Main();
     CqlSessionBuilder builder = CqlSession.builder();
     // Set the host and port of the Cassandra server here
-    builder.addContactPoint(new InetSocketAddress("${cassandra-host}", ${cassandra-port}));
-    try (CqlSession session = builder.build()) {
+    builder.addContactPoint(new InetSocketAddress("127.0.0.1", 9042));
+    try (CqlSession session = builder.build();
+        LineNumberReader commandLine = new LineNumberReader(new InputStreamReader(System.in))) {
       ResultSet rs = session.execute("SELECT release_version FROM system.local");
       LOG.info("Cassandra release version: {}", rs.one().getString(0));
+      // run the cqlsh demo
+      main.cqlshLite(session, commandLine);
+      // demo exited
+      System.out.println();
+      System.out.println("Done. Good Bye!");
+    } catch (IOException ioe) {
+      ioe.printStackTrace(main.output);
     }
   }
 }
