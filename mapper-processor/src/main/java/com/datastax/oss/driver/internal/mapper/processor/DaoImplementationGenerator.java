6644fdee61226dcd1b4e0a30dcde8e374e8f3d49
olim7t <omichallat+github@gmail.com>
2018-12-27 17:50:51 +0100

    Initialize object mapper structure
    Generation of basic Mapper and Dao classes, no query methods yet.

---
+++ mapper-processor/src/main/java/com/datastax/oss/driver/internal/mapper/processor/DaoImplementationGenerator.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.internal.mapper.processor;
+
+import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.mapper.annotations.Dao;
+import com.datastax.oss.driver.internal.core.util.concurrent.BlockingOperation;
+import com.datastax.oss.driver.internal.core.util.concurrent.CompletableFutures;
+import com.datastax.oss.driver.internal.mapper.MapperContext;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeSpec;
+import java.util.concurrent.CompletableFuture;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+
+/** Generates the implementation of a {@link Dao}-annotated interface. */
+public class DaoImplementationGenerator extends FileGenerator {
+
+  private final TypeElement interfaceElement;
+  private final ClassName implementationName;
+
+  public DaoImplementationGenerator(TypeElement interfaceElement, GenerationContext context) {
+    super(context);
+    this.interfaceElement = interfaceElement;
+    implementationName = GeneratedNames.daoImplementation(interfaceElement);
+  }
+
+  @Override
+  protected String getFileName() {
+    return implementationName.packageName() + "." + implementationName.simpleName();
+  }
+
+  @Override
+  protected JavaFile.Builder getContents() {
+
+    TypeSpec.Builder classBuilder =
+        TypeSpec.classBuilder(implementationName)
+            .addJavadoc(JAVADOC_GENERATED_WARNING)
+            .addModifiers(Modifier.PUBLIC)
+            .addSuperinterface(ClassName.get(interfaceElement))
+            .addField(
+                FieldSpec.builder(MapperContext.class, "context", Modifier.PRIVATE, Modifier.FINAL)
+                    .build())
+            .addField(
+                FieldSpec.builder(
+                        CqlIdentifier.class, "keyspaceId", Modifier.PRIVATE, Modifier.FINAL)
+                    .build())
+            .addField(
+                FieldSpec.builder(CqlIdentifier.class, "tableId", Modifier.PRIVATE, Modifier.FINAL)
+                    .build());
+
+    MethodSpec.Builder initAsyncBuilder =
+        MethodSpec.methodBuilder("initAsync")
+            .returns(
+                ParameterizedTypeName.get(
+                    ClassName.get(CompletableFuture.class), ClassName.get(interfaceElement)))
+            .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+            .addParameter(MapperContext.class, "context")
+            .addParameter(CqlIdentifier.class, "keyspaceId")
+            .addParameter(CqlIdentifier.class, "tableId")
+            .addStatement(
+                "return $T.completedFuture(new $T(context, keyspaceId, tableId))",
+                CompletableFuture.class,
+                implementationName);
+
+    MethodSpec.Builder initBuilder =
+        MethodSpec.methodBuilder("init")
+            .returns(ClassName.get(interfaceElement))
+            .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+            .addParameter(MapperContext.class, "context")
+            .addParameter(CqlIdentifier.class, "keyspaceId")
+            .addParameter(CqlIdentifier.class, "tableId")
+            .addStatement("$T.checkNotDriverThread()", BlockingOperation.class)
+            .addStatement(
+                "return $T.getUninterruptibly(initAsync(context, keyspaceId, tableId))",
+                CompletableFutures.class);
+
+    MethodSpec.Builder constructorBuilder =
+        MethodSpec.constructorBuilder()
+            .addModifiers(Modifier.PRIVATE)
+            .addParameter(MapperContext.class, "context")
+            .addParameter(CqlIdentifier.class, "keyspaceId")
+            .addParameter(CqlIdentifier.class, "tableId")
+            .addStatement("this.context = context")
+            .addStatement("this.keyspaceId = keyspaceId")
+            .addStatement("this.tableId = tableId");
+
+    classBuilder.addMethod(initAsyncBuilder.build());
+    classBuilder.addMethod(initBuilder.build());
+    classBuilder.addMethod(constructorBuilder.build());
+
+    return JavaFile.builder(implementationName.packageName(), classBuilder.build());
+  }
+}
---
+++ mapper-processor/src/main/java/com/datastax/oss/driver/internal/mapper/processor/MapperProcessor.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.internal.mapper.processor;
+
+import com.datastax.oss.driver.api.mapper.annotations.Dao;
+import com.datastax.oss.driver.api.mapper.annotations.Mapper;
+import com.datastax.oss.driver.shaded.guava.common.base.Strings;
+import com.datastax.oss.driver.shaded.guava.common.collect.ImmutableSet;
+import com.google.auto.service.AutoService;
+import com.google.common.base.Throwables;
+import java.lang.annotation.Annotation;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+
+@AutoService(Processor.class)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class MapperProcessor extends AbstractProcessor {
+
+  private static final String INDENT_AMOUNT_OPTION = "com.datastax.oss.driver.mapper.indent";
+  private static final String INDENT_WITH_TABS_OPTION =
+      "com.datastax.oss.driver.mapper.indentWithTabs";
+
+  private DecoratedMessager messager;
+  private Types typeUtils;
+  private Elements elementUtils;
+  private Filer filer;
+  private String indent;
+
+  @Override
+  public synchronized void init(ProcessingEnvironment processingEnvironment) {
+    super.init(processingEnvironment);
+    messager = new DecoratedMessager(processingEnvironment.getMessager());
+    typeUtils = processingEnvironment.getTypeUtils();
+    elementUtils = processingEnvironment.getElementUtils();
+    filer = processingEnvironment.getFiler();
+    indent = computeIndent(processingEnvironment.getOptions());
+  }
+
+  @Override
+  public boolean process(
+      Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
+    GenerationContext context =
+        new GenerationContext(messager, typeUtils, elementUtils, filer, indent);
+
+    processAnnotatedInterfaces(
+        roundEnvironment, Dao.class, e -> new DaoImplementationGenerator(e, context).generate());
+    processAnnotatedInterfaces(
+        roundEnvironment, Mapper.class, e -> new MapperGenerator(e, context).generate());
+    return true;
+  }
+
+  private void processAnnotatedInterfaces(
+      RoundEnvironment roundEnvironment,
+      Class<? extends Annotation> annotationClass,
+      Consumer<TypeElement> action) {
+    for (Element element : roundEnvironment.getElementsAnnotatedWith(annotationClass)) {
+      if (element.getKind() != ElementKind.INTERFACE) {
+        messager.error(
+            element, "Only interfaces can be annotated with %s", annotationClass.getSimpleName());
+      } else {
+        // Safe cast given that we checked the kind above
+        TypeElement typeElement = (TypeElement) element;
+        try {
+          action.accept(typeElement);
+        } catch (Exception e) {
+          messager.error(
+              element,
+              "Unexpected error while writing generated code: %s",
+              Throwables.getStackTraceAsString(e));
+        }
+      }
+    }
+  }
+
+  @Override
+  public Set<String> getSupportedAnnotationTypes() {
+    return ImmutableSet.of(Mapper.class.getName(), Dao.class.getName());
+  }
+
+  @Override
+  public Set<String> getSupportedOptions() {
+    return ImmutableSet.of(INDENT_AMOUNT_OPTION, INDENT_WITH_TABS_OPTION);
+  }
+
+  private String computeIndent(Map<String, String> options) {
+    boolean tabs = options.containsKey(INDENT_WITH_TABS_OPTION);
+    String amountSpec = options.get(INDENT_AMOUNT_OPTION);
+    if (amountSpec != null) {
+      try {
+        int amount = Integer.parseInt(amountSpec);
+        return Strings.repeat(tabs ? "\t" : " ", amount);
+      } catch (NumberFormatException e) {
+        messager.warn(
+            "Could not parse %s: expected a number, got '%s'. Defaulting to %s.",
+            INDENT_AMOUNT_OPTION, amountSpec, tabs ? "1 tab" : "2 spaces");
+      }
+    }
+    return tabs ? "\t" : "  ";
+  }
+}
