ceb2647040425d75e7eb6d98a6bf9d5a406cd445
olim7t <omichallat+github@gmail.com>
2019-01-20 16:49:09 -0800

    Refactor for better modularity

---
+++ mapper-processor/src/main/java/com/datastax/oss/driver/internal/mapper/processor/CodeGeneratorFactory.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.internal.mapper.processor;
+
+import com.datastax.oss.driver.api.mapper.annotations.Dao;
+import com.datastax.oss.driver.api.mapper.annotations.Mapper;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+
+public interface CodeGeneratorFactory {
+
+  /**
+   * All the types derived from a {@link Mapper}-annotated interface.
+   *
+   * <p>By default, this calls {@link #newMapperImplementation(TypeElement)} and {@link
+   * #newMapperBuilder(TypeElement)}.
+   */
+  CodeGenerator newMapper(TypeElement interfaceElement);
+
+  /**
+   * The implementation of a {@link Mapper}-annotated interface.
+   *
+   * <p>By default, this calls {@link #newDaoMethodFactory(ExecutableElement)} for each DAO factory
+   * method.
+   */
+  CodeGenerator newMapperImplementation(TypeElement interfaceElement);
+
+  /** A method of a {@link Mapper}-annotated interface that produces DAO instances. */
+  PartialClassGenerator newDaoMethodFactory(ExecutableElement methodElement);
+
+  /** The builder associated to a {@link Mapper}-annotated interface. */
+  CodeGenerator newMapperBuilder(TypeElement interfaceElement);
+
+  /** The implementation of a {@link Dao}-annotated interface. */
+  CodeGenerator newDao(TypeElement interfaceElement);
+}
--- mapper-processor/src/main/java/com/datastax/oss/driver/internal/mapper/processor/mapper/DaoFactoryMethodGenerator.java
+++ mapper-processor/src/main/java/com/datastax/oss/driver/internal/mapper/processor/mapper/DaoFactoryMethodGenerator.java
@@ -13,81 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.datastax.oss.driver.internal.mapper.processor;
+package com.datastax.oss.driver.internal.mapper.processor.mapper;
 
 import com.datastax.oss.driver.api.core.CqlIdentifier;
-import com.datastax.oss.driver.api.mapper.annotations.Dao;
 import com.datastax.oss.driver.api.mapper.annotations.DaoKeyspace;
 import com.datastax.oss.driver.api.mapper.annotations.DaoTable;
 import com.datastax.oss.driver.internal.mapper.DaoCacheKey;
+import com.datastax.oss.driver.internal.mapper.processor.GeneratedNames;
+import com.datastax.oss.driver.internal.mapper.processor.PartialClassGenerator;
+import com.datastax.oss.driver.internal.mapper.processor.ProcessorContext;
+import com.datastax.oss.driver.internal.mapper.processor.SkipGenerationException;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
-import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 
-/** Generate the methods of the main mapper that produce DAOs. */
 public class DaoFactoryMethodGenerator implements PartialClassGenerator {
-
-  /**
-   * Tries to create a generator for the given method if it matches an expected signature, otherwise
-   * returns null.
-   */
-  public static DaoFactoryMethodGenerator newInstance(
-      ExecutableElement methodElement, GenerationContext context) {
-    TypeMirror returnType = methodElement.getReturnType();
-    if (returnType.getKind() != TypeKind.DECLARED) {
-      return null;
-    }
-    DeclaredType declaredReturnType = (DeclaredType) returnType;
-    if (declaredReturnType.getTypeArguments().isEmpty()) {
-      Element returnTypeElement = declaredReturnType.asElement();
-      return (returnTypeElement.getKind() != ElementKind.INTERFACE
-              || returnTypeElement.getAnnotation(Dao.class) == null)
-          ? null
-          : new DaoFactoryMethodGenerator(
-              methodElement,
-              GeneratedNames.daoImplementation(((TypeElement) returnTypeElement)),
-              false,
-              context);
-    } else if (isFuture(declaredReturnType.asElement(), context)
-        && declaredReturnType.getTypeArguments().size() == 1) {
-      TypeMirror typeArgument = declaredReturnType.getTypeArguments().get(0);
-      if (typeArgument.getKind() != TypeKind.DECLARED) {
-        return null;
-      }
-      Element typeArgumentElement = ((DeclaredType) typeArgument).asElement();
-      return (typeArgumentElement.getKind() != ElementKind.INTERFACE
-              || typeArgumentElement.getAnnotation(Dao.class) == null)
-          ? null
-          : new DaoFactoryMethodGenerator(
-              methodElement,
-              GeneratedNames.daoImplementation(((TypeElement) typeArgumentElement)),
-              true,
-              context);
-    } else {
-      return null;
-    }
-  }
-
-  private static boolean isFuture(Element element, GenerationContext context) {
-    return context.isSame(element, CompletionStage.class)
-        || context.isSame(element, CompletableFuture.class);
-  }
 
   private final ExecutableElement methodElement;
   private final CharSequence keyspaceArgumentName;
@@ -97,11 +46,11 @@ public class DaoFactoryMethodGenerator implements PartialClassGenerator {
   private final boolean isAsync;
   private final boolean isCachedByKeyspaceAndTable;
 
-  private DaoFactoryMethodGenerator(
+  public DaoFactoryMethodGenerator(
       ExecutableElement methodElement,
       ClassName daoImplementationName,
       boolean isAsync,
-      GenerationContext context) {
+      ProcessorContext context) {
     this.methodElement = methodElement;
     this.daoImplementationName = daoImplementationName;
     this.isAsync = isAsync;
@@ -136,7 +85,7 @@ public class DaoFactoryMethodGenerator implements PartialClassGenerator {
       VariableElement candidate,
       VariableElement previous,
       Class<?> annotation,
-      GenerationContext context) {
+      ProcessorContext context) {
     if (previous != null) {
       context
           .getMessager()
@@ -147,7 +96,8 @@ public class DaoFactoryMethodGenerator implements PartialClassGenerator {
       throw new SkipGenerationException();
     }
     TypeMirror type = candidate.asType();
-    if (!context.isSame(type, String.class) && !context.isSame(type, CqlIdentifier.class)) {
+    if (!context.getClassUtils().isSame(type, String.class)
+        && !context.getClassUtils().isSame(type, CqlIdentifier.class)) {
       context
           .getMessager()
           .error(
