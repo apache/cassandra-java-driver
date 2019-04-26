e7b31b5d297d171f4573b3213a52c9b0d616a280
olim7t <omichallat+github@gmail.com>
2019-04-25 18:58:34 -0700

    Improve code reuse in mapper unit tests

--- mapper-processor/src/test/java/com/datastax/oss/driver/internal/mapper/processor/entity/EntityAnnotationTest.java
+++ mapper-processor/src/test/java/com/datastax/oss/driver/internal/mapper/processor/entity/EntityAnnotationTest.java
@@ -13,11 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.datastax.oss.driver.internal.mapper.processor;
+package com.datastax.oss.driver.internal.mapper.processor.entity;
 
 import static com.google.testing.compile.CompilationSubject.assertThat;
 
 import com.datastax.oss.driver.api.mapper.annotations.Entity;
+import com.datastax.oss.driver.internal.mapper.processor.MapperProcessorTest;
 import com.google.testing.compile.Compilation;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.MethodSpec;
@@ -65,14 +66,12 @@ public class EntityAnnotationTest extends MapperProcessorTest {
 
   @Test
   public void should_fail_on_interface() {
-    Compilation compilation =
-        compileWithMapperProcessor(
-            "test",
-            TypeSpec.interfaceBuilder(ClassName.get("test", "Foo"))
-                .addModifiers(Modifier.PUBLIC)
-                .addAnnotation(Entity.class)
-                .build());
-
-    assertThat(compilation).hadErrorContaining("Only CLASS elements can be annotated with Entity");
+    should_fail_with_expected_error(
+        "Only CLASS elements can be annotated with Entity",
+        "test",
+        TypeSpec.interfaceBuilder(ClassName.get("test", "Foo"))
+            .addModifiers(Modifier.PUBLIC)
+            .addAnnotation(Entity.class)
+            .build());
   }
 }
