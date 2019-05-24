bec9be19423aa23c7a4aa02e176c935c4d9c946a
Andrew Tolbert <andrew.tolbert@datastax.com>
2019-05-24 13:46:16 -0500

    JAVA-2255: Add annotation to indicate a property is transient (#1239)
    * JAVA-2255: Add annotation to indicate a property is transient
    
    Motivation:
    
    It is sometimes desirable for properties of an entity to not be
    considered by an object mapper.
    
    Modifications:
    
    Introduced a `@Transient` annotation that when applied to a field or
    getter, causes the mapper to not include this field for generated
    queries.
    
    Added a `@TransientProperties` keyword for marking properties
    as transient at entity level.
    
    In addition, if a field includes the 'transient' keyword, it is also not
    considered.
    
    Attempt to handle conflicting annotations in a consistent way.
    
    Result:
    
    properties including a `@Transient` annotation, included in
    `TransientProperties` or having a field with transient keyword
    will not be considered by the mapper.

---
+++ mapper-runtime/src/main/java/com/datastax/oss/driver/api/mapper/annotations/Transient.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.api.mapper.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotates the field or getter of an {@link Entity} property, to indicate that it will not be
+ * mapped to any column (neither during reads nor writes).
+ *
+ * <p>Example:
+ *
+ * <pre>
+ * &#64;Transient private int notAColumn;
+ * </pre>
+ *
+ * This information is used by the mapper processor to exclude the property from generated queries.
+ *
+ * <p>Please note that {@code Transient} takes precedence over {@link PartitionKey} and {@link
+ * ClusteringColumn} annotations.
+ */
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.CLASS)
+public @interface Transient {}
