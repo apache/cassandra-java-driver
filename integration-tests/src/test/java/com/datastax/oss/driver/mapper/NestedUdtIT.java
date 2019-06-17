seequest/java-driver

JAVA-2129: Handle mapped collections of UDTs

integration-tests/src/test/java/com/datastax/oss/driver/mapper/NestedUdtIT.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.mapper;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.testinfra.ccm.CcmRule;
+import com.datastax.oss.driver.api.testinfra.session.SessionRule;
+import com.datastax.oss.driver.categories.ParallelizableTests;
+import com.datastax.oss.driver.mapper.model.udts.Container;
+import com.datastax.oss.driver.mapper.model.udts.ContainerDao;
+import com.datastax.oss.driver.mapper.model.udts.UdtsFixtures;
+import com.datastax.oss.driver.mapper.model.udts.UdtsMapper;
+import com.datastax.oss.driver.mapper.model.udts.UdtsMapperBuilder;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+@Category(ParallelizableTests.class)
+public class NestedUdtIT {
+
+  private static CcmRule ccm = CcmRule.getInstance();
+
+  private static SessionRule<CqlSession> sessionRule = SessionRule.builder(ccm).build();
+
+  @ClassRule public static TestRule chain = RuleChain.outerRule(ccm).around(sessionRule);
+
+  private static ContainerDao containerDao;
+
+  @BeforeClass
+  public static void setup() {
+    CqlSession session = sessionRule.session();
+
+    for (String query : UdtsFixtures.createStatements()) {
+      session.execute(
+          SimpleStatement.builder(query).withExecutionProfile(sessionRule.slowProfile()).build());
+    }
+
+    UdtsMapper udtsMapper = new UdtsMapperBuilder(session).build();
+    containerDao = udtsMapper.containerDao(sessionRule.keyspace());
+  }
+
+  @Test
+  public void should_insert_entity_with_nested_udts() {
+    // Given
+    CqlSession session = sessionRule.session();
+
+    // When
+    Container container = UdtsFixtures.SAMPLE_CONTAINER.entity;
+    containerDao.save(container);
+    Row row =
+        session
+            .execute(
+                SimpleStatement.newInstance(
+                    "SELECT * FROM container WHERE id = ?", container.getId()))
+            .one();
+
+    // Then
+    UdtsFixtures.SAMPLE_CONTAINER.assertMatches(row);
+  }
+}

mapper-processor/src/main/java/com/datastax/oss/driver/internal/mapper/processor/util/generation/PropertyType.java
@@ -1,0 +1,183 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.internal.mapper.processor.util.generation;
+
+import com.datastax.oss.driver.api.core.data.GettableByName;
+import com.datastax.oss.driver.api.core.data.SettableByName;
+import com.datastax.oss.driver.api.core.data.UdtValue;
+import com.datastax.oss.driver.api.mapper.annotations.Entity;
+import com.datastax.oss.driver.internal.mapper.processor.ProcessorContext;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+
+/**
+ * Wraps the declared type of an entity property (or DAO method parameter) that will be injected
+ * into a {@link SettableByName}, or extracted from a {@link GettableByName}.
+ *
+ * <p>The goal is to detect if the type contains other mapped entities, that must be translated into
+ * UDT values.
+ */
+public abstract class PropertyType {
+
+  private static final ClassName UDT_VALUE_CLASS_NAME = ClassName.get(UdtValue.class);
+  public static final ClassName LIST_CLASS_NAME = ClassName.get(List.class);
+  public static final ClassName SET_CLASS_NAME = ClassName.get(Set.class);
+  public static final ClassName MAP_CLASS_NAME = ClassName.get(Map.class);
+
+  public static PropertyType parse(TypeMirror typeMirror, ProcessorContext context) {
+    if (typeMirror.getKind() == TypeKind.DECLARED) {
+      DeclaredType declaredType = (DeclaredType) typeMirror;
+      if (declaredType.asElement().getAnnotation(Entity.class) != null) {
+        return new SingleEntity(declaredType);
+      } else if (context.getClassUtils().isList(declaredType)) {
+        PropertyType elementType = parse(declaredType.getTypeArguments().get(0), context);
+        return (elementType instanceof Simple)
+            ? new Simple(typeMirror)
+            : new EntityList(elementType);
+      } else if (context.getClassUtils().isSet(declaredType)) {
+        PropertyType elementType = parse(declaredType.getTypeArguments().get(0), context);
+        return (elementType instanceof Simple)
+            ? new Simple(typeMirror)
+            : new EntitySet(elementType);
+      } else if (context.getClassUtils().isMap(declaredType)) {
+        PropertyType keyType = parse(declaredType.getTypeArguments().get(0), context);
+        PropertyType valueType = parse(declaredType.getTypeArguments().get(1), context);
+        return (keyType instanceof Simple && valueType instanceof Simple)
+            ? new Simple(typeMirror)
+            : new EntityMap(keyType, valueType);
+      }
+    }
+    return new Simple(typeMirror);
+  }
+
+  public abstract TypeName asTypeName();
+
+  /**
+   * Returns the name of the type we will convert to before saving to the database; that is,
+   * replacing every entity class by {@code UdtValue}.
+   */
+  public abstract TypeName asConvertedTypeName();
+
+  /**
+   * A type that does not contain any mapped entity.
+   *
+   * <p>Note that it can still be a collection, for example {@code Map<String, List<Integer>>}.
+   */
+  public static class Simple extends PropertyType {
+    public final TypeName typeName;
+
+    public Simple(TypeMirror typeMirror) {
+      this.typeName = ClassName.get(typeMirror);
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return typeName;
+    }
+
+    @Override
+    public TypeName asConvertedTypeName() {
+      return typeName;
+    }
+  }
+
+  /** A mapped entity. */
+  public static class SingleEntity extends PropertyType {
+    public final ClassName entityName;
+
+    public SingleEntity(DeclaredType declaredType) {
+      this.entityName = (ClassName) TypeName.get(declaredType);
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return entityName;
+    }
+
+    @Override
+    public TypeName asConvertedTypeName() {
+      return UDT_VALUE_CLASS_NAME;
+    }
+  }
+
+  /** A list of another non-simple type. */
+  public static class EntityList extends PropertyType {
+    public final PropertyType elementType;
+
+    public EntityList(PropertyType elementType) {
+      this.elementType = elementType;
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return ParameterizedTypeName.get(LIST_CLASS_NAME, elementType.asTypeName());
+    }
+
+    @Override
+    public TypeName asConvertedTypeName() {
+      return ParameterizedTypeName.get(LIST_CLASS_NAME, elementType.asConvertedTypeName());
+    }
+  }
+
+  /** A set of another non-simple type. */
+  public static class EntitySet extends PropertyType {
+    public final PropertyType elementType;
+
+    public EntitySet(PropertyType elementType) {
+      this.elementType = elementType;
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return ParameterizedTypeName.get(SET_CLASS_NAME, elementType.asTypeName());
+    }
+
+    @Override
+    public TypeName asConvertedTypeName() {
+      return ParameterizedTypeName.get(SET_CLASS_NAME, elementType.asConvertedTypeName());
+    }
+  }
+
+  /** A map where either the key type, the value type, or both, are non-simple types. */
+  public static class EntityMap extends PropertyType {
+    public final PropertyType keyType;
+    public final PropertyType valueType;
+
+    public EntityMap(PropertyType keyType, PropertyType valueType) {
+      this.keyType = keyType;
+      this.valueType = valueType;
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return ParameterizedTypeName.get(
+          MAP_CLASS_NAME, keyType.asTypeName(), valueType.asTypeName());
+    }
+
+    @Override
+    public TypeName asConvertedTypeName() {
+      return ParameterizedTypeName.get(
+          MAP_CLASS_NAME, keyType.asConvertedTypeName(), valueType.asConvertedTypeName());
+    }
+  }
+}

apache/cassandra-java-driver

integration-tests/src/test/java/com/datastax/oss/driver/mapper/NestedUdtIT.java
@@ -1,0 +1,101 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.testinfra.ccm.CcmRule;
+import com.datastax.oss.driver.api.testinfra.session.SessionRule;
+import com.datastax.oss.driver.categories.ParallelizableTests;
+import com.datastax.oss.driver.mapper.model.udts.Container;
+import com.datastax.oss.driver.mapper.model.udts.ContainerDao;
+import com.datastax.oss.driver.mapper.model.udts.UdtsFixtures;
+import com.datastax.oss.driver.mapper.model.udts.UdtsMapper;
+import com.datastax.oss.driver.mapper.model.udts.UdtsMapperBuilder;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+@Category(ParallelizableTests.class)
+public class NestedUdtIT {
+
+  private static CcmRule ccm = CcmRule.getInstance();
+
+  private static SessionRule<CqlSession> sessionRule = SessionRule.builder(ccm).build();
+
+  @ClassRule public static TestRule chain = RuleChain.outerRule(ccm).around(sessionRule);
+
+  private static ContainerDao containerDao;
+
+  @BeforeClass
+  public static void setup() {
+    CqlSession session = sessionRule.session();
+
+    for (String query : UdtsFixtures.createStatements()) {
+      session.execute(
+          SimpleStatement.builder(query).setExecutionProfile(sessionRule.slowProfile()).build());
+    }
+
+    UdtsMapper udtsMapper = new UdtsMapperBuilder(session).build();
+    containerDao = udtsMapper.containerDao(sessionRule.keyspace());
+  }
+
+  @Test
+  public void should_insert_entity_with_nested_udts() {
+    // Given
+    CqlSession session = sessionRule.session();
+
+    // When
+    Container container = UdtsFixtures.SAMPLE_CONTAINER.entity;
+    containerDao.save(container);
+    Row row =
+        session
+            .execute(
+                SimpleStatement.newInstance(
+                    "SELECT * FROM container WHERE id = ?", container.getId()))
+            .one();
+
+    // Then
+    UdtsFixtures.SAMPLE_CONTAINER.assertMatches(row);
+  }
+
+  @Test
+  public void should_get_entity_with_nested_udts() {
+    // Given
+    CqlSession session = sessionRule.session();
+
+    // When
+    Container insertedContainer = UdtsFixtures.SAMPLE_CONTAINER.entity;
+    containerDao.save(insertedContainer);
+    // TODO maybe replace with a Dao @Select once that is implemented
+    Row row =
+        session
+            .execute(
+                SimpleStatement.newInstance(
+                    "SELECT * FROM container WHERE id = ?", insertedContainer.getId()))
+            .one();
+    Container retrievedContainer = containerDao.get(row);
+
+    // Then
+    assertThat(retrievedContainer).isEqualTo(insertedContainer);
+  }
+}

mapper-processor/src/main/java/com/datastax/oss/driver/internal/mapper/processor/util/generation/PropertyType.java
@@ -1,0 +1,183 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.internal.mapper.processor.util.generation;
+
+import com.datastax.oss.driver.api.core.data.GettableByName;
+import com.datastax.oss.driver.api.core.data.SettableByName;
+import com.datastax.oss.driver.api.core.data.UdtValue;
+import com.datastax.oss.driver.api.mapper.annotations.Entity;
+import com.datastax.oss.driver.internal.mapper.processor.ProcessorContext;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+
+/**
+ * Wraps the declared type of an entity property (or DAO method parameter) that will be injected
+ * into a {@link SettableByName}, or extracted from a {@link GettableByName}.
+ *
+ * <p>The goal is to detect if the type contains other mapped entities, that must be translated into
+ * UDT values.
+ */
+public abstract class PropertyType {
+
+  private static final ClassName UDT_VALUE_CLASS_NAME = ClassName.get(UdtValue.class);
+  public static final ClassName LIST_CLASS_NAME = ClassName.get(List.class);
+  public static final ClassName SET_CLASS_NAME = ClassName.get(Set.class);
+  public static final ClassName MAP_CLASS_NAME = ClassName.get(Map.class);
+
+  public static PropertyType parse(TypeMirror typeMirror, ProcessorContext context) {
+    if (typeMirror.getKind() == TypeKind.DECLARED) {
+      DeclaredType declaredType = (DeclaredType) typeMirror;
+      if (declaredType.asElement().getAnnotation(Entity.class) != null) {
+        return new SingleEntity(declaredType);
+      } else if (context.getClassUtils().isList(declaredType)) {
+        PropertyType elementType = parse(declaredType.getTypeArguments().get(0), context);
+        return (elementType instanceof Simple)
+            ? new Simple(typeMirror)
+            : new EntityList(elementType);
+      } else if (context.getClassUtils().isSet(declaredType)) {
+        PropertyType elementType = parse(declaredType.getTypeArguments().get(0), context);
+        return (elementType instanceof Simple)
+            ? new Simple(typeMirror)
+            : new EntitySet(elementType);
+      } else if (context.getClassUtils().isMap(declaredType)) {
+        PropertyType keyType = parse(declaredType.getTypeArguments().get(0), context);
+        PropertyType valueType = parse(declaredType.getTypeArguments().get(1), context);
+        return (keyType instanceof Simple && valueType instanceof Simple)
+            ? new Simple(typeMirror)
+            : new EntityMap(keyType, valueType);
+      }
+    }
+    return new Simple(typeMirror);
+  }
+
+  public abstract TypeName asTypeName();
+
+  /**
+   * Returns the name of the type we will convert to before saving to the database; that is,
+   * replacing every entity class by {@code UdtValue}.
+   */
+  public abstract TypeName asRawTypeName();
+
+  /**
+   * A type that does not contain any mapped entity.
+   *
+   * <p>Note that it can still be a collection, for example {@code Map<String, List<Integer>>}.
+   */
+  public static class Simple extends PropertyType {
+    public final TypeName typeName;
+
+    public Simple(TypeMirror typeMirror) {
+      this.typeName = ClassName.get(typeMirror);
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return typeName;
+    }
+
+    @Override
+    public TypeName asRawTypeName() {
+      return typeName;
+    }
+  }
+
+  /** A mapped entity. */
+  public static class SingleEntity extends PropertyType {
+    public final ClassName entityName;
+
+    public SingleEntity(DeclaredType declaredType) {
+      this.entityName = (ClassName) TypeName.get(declaredType);
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return entityName;
+    }
+
+    @Override
+    public TypeName asRawTypeName() {
+      return UDT_VALUE_CLASS_NAME;
+    }
+  }
+
+  /** A list of another non-simple type. */
+  public static class EntityList extends PropertyType {
+    public final PropertyType elementType;
+
+    public EntityList(PropertyType elementType) {
+      this.elementType = elementType;
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return ParameterizedTypeName.get(LIST_CLASS_NAME, elementType.asTypeName());
+    }
+
+    @Override
+    public TypeName asRawTypeName() {
+      return ParameterizedTypeName.get(LIST_CLASS_NAME, elementType.asRawTypeName());
+    }
+  }
+
+  /** A set of another non-simple type. */
+  public static class EntitySet extends PropertyType {
+    public final PropertyType elementType;
+
+    public EntitySet(PropertyType elementType) {
+      this.elementType = elementType;
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return ParameterizedTypeName.get(SET_CLASS_NAME, elementType.asTypeName());
+    }
+
+    @Override
+    public TypeName asRawTypeName() {
+      return ParameterizedTypeName.get(SET_CLASS_NAME, elementType.asRawTypeName());
+    }
+  }
+
+  /** A map where either the key type, the value type, or both, are non-simple types. */
+  public static class EntityMap extends PropertyType {
+    public final PropertyType keyType;
+    public final PropertyType valueType;
+
+    public EntityMap(PropertyType keyType, PropertyType valueType) {
+      this.keyType = keyType;
+      this.valueType = valueType;
+    }
+
+    @Override
+    public TypeName asTypeName() {
+      return ParameterizedTypeName.get(
+          MAP_CLASS_NAME, keyType.asTypeName(), valueType.asTypeName());
+    }
+
+    @Override
+    public TypeName asRawTypeName() {
+      return ParameterizedTypeName.get(
+          MAP_CLASS_NAME, keyType.asRawTypeName(), valueType.asRawTypeName());
+    }
+  }
+}