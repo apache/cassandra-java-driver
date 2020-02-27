c3a6e6b30c4be2ad72986a5dd1e458fc13c2d2a1
olim7t <omichallat+github@gmail.com>
2020-02-27 15:23:12 -0800

    Reformat NodeState javadocs
    Bullet lists in the title line do not render well in the HTML output.

--- core/src/main/java/com/datastax/oss/driver/api/core/metadata/NodeState.java
+++ core/src/main/java/com/datastax/oss/driver/api/core/metadata/NodeState.java
@@ -30,25 +30,17 @@ public enum NodeState {
    */
   UNKNOWN,
   /**
-   * A node is considered up in either of the following situations:
-   *
-   * <ul>
-   *   <li>the driver has at least one active connection to the node.
-   *   <li>the driver is not actively trying to connect to the node (because it's ignored by the
-   *       {@link LoadBalancingPolicy}), but it has received a topology event indicating that the
-   *       node is up.
-   * </ul>
+   * A node is considered up in either of the following situations: 1) the driver has at least one
+   * active connection to the node, or 2) the driver is not actively trying to connect to the node
+   * (because it's ignored by the {@link LoadBalancingPolicy}), but it has received a topology event
+   * indicating that the node is up.
    */
   UP,
   /**
-   * A node is considered down in either of the following situations:
-   *
-   * <ul>
-   *   <li>the driver has lost all connections to the node (and is currently trying to reconnect).
-   *   <li>the driver is not actively trying to connect to the node (because it's ignored by the
-   *       {@link LoadBalancingPolicy}), but it has received a topology event indicating that the
-   *       node is down.
-   * </ul>
+   * A node is considered down in either of the following situations: 1) the driver has lost all
+   * connections to the node (and is currently trying to reconnect), or 2) the driver is not
+   * actively trying to connect to the node (because it's ignored by the {@link
+   * LoadBalancingPolicy}), but it has received a topology event indicating that the node is down.
    */
   DOWN,
   /**
