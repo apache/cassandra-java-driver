9056e722a16d1a9fd21ad8561fe61c5d5fd47ed4
Bret McGuire <bret.mcguire@datastax.com>
2020-05-12 01:54:00 -0500

    Add platform check to indicate that we're available only for Linux and/or Darwin

--- core/src/main/java/com/datastax/oss/driver/internal/core/os/GraalLibc.java
+++ core/src/main/java/com/datastax/oss/driver/internal/core/os/GraalLibc.java
@@ -15,24 +15,80 @@
  */
 package com.datastax.oss.driver.internal.core.os;
 
+import java.util.Locale;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GraalLibc implements Libc {
 
+  private static final Logger LOG = LoggerFactory.getLogger(GraalLibc.class);
+
+  private static final Locale LOCALE = Locale.ENGLISH;
+
+  private static final String MAC_PLATFORM_STR = "mac".toLowerCase(LOCALE);
+  private static final String DARWIN_PLATFORM_STR = "darwin".toLowerCase(LOCALE);
+  private static final String LINUX_PLATFORM_STR = "linux".toLowerCase(LOCALE);
+
+  private final boolean available = checkAvailability();
+
+  /* This method is adapted from of jnr.ffi.Platform.determineOS() in jnr-ffi version 2.1.10. **/
+  private boolean checkPlatform() {
+
+    String osName = System.getProperty("os.name").split(" ", -1)[0];
+    String compareStr = osName.toLowerCase(Locale.ENGLISH);
+    return compareStr.startsWith(MAC_PLATFORM_STR)
+        || compareStr.startsWith(DARWIN_PLATFORM_STR)
+        || compareStr.startsWith(LINUX_PLATFORM_STR);
+  }
+
+  private boolean checkAvailability() {
+
+    if (!checkPlatform()) {
+      return false;
+    }
+
+    try {
+      getpidRaw();
+    } catch (Throwable t) {
+
+      LOG.debug("Error calling getpid()", t);
+      return false;
+    }
+
+    try {
+      gettimeofdayRaw();
+    } catch (Throwable t) {
+
+      LOG.debug("Error calling gettimeofday()", t);
+      return false;
+    }
+
+    return true;
+  }
+
   @Override
   public boolean available() {
-    return true;
+    return this.available;
   }
 
   /* Substrate includes a substitution for Linux + Darwin which redefines System.nanoTime() to use
    * gettimeofday() (unless platform-specific higher-res clocks are available, which is even better). */
   @Override
   public Optional<Long> gettimeofday() {
-    return Optional.of(Math.round(System.nanoTime() / 1_000d));
+    return this.available ? Optional.of(gettimeofdayRaw()) : Optional.empty();
+  }
+
+  private long gettimeofdayRaw() {
+    return Math.round(System.nanoTime() / 1_000d);
   }
 
   @Override
   public Optional<Integer> getpid() {
-    return Optional.of(GraalGetpid.getpid());
+    return this.available ? Optional.of(getpidRaw()) : Optional.empty();
+  }
+
+  private int getpidRaw() {
+    return GraalGetpid.getpid();
   }
 }
