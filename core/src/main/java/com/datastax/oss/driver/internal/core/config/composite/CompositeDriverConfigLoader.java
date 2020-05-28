bfbf82734c734e311cc9869ec351cbd3f127df4e
olim7t <omichallat+github@gmail.com>
2020-05-27 21:18:29 -0700

    JAVA-2793: Add composite config loader

---
+++ core/src/main/java/com/datastax/oss/driver/internal/core/config/composite/CompositeDriverConfigLoader.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.internal.core.config.composite;
+
+import com.datastax.oss.driver.api.core.config.DriverConfig;
+import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
+import com.datastax.oss.driver.api.core.context.DriverContext;
+import com.datastax.oss.driver.internal.core.util.concurrent.CompletableFutures;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+public class CompositeDriverConfigLoader implements DriverConfigLoader {
+
+  private final DriverConfigLoader primaryConfigLoader;
+  private final DriverConfigLoader fallbackConfigLoader;
+
+  public CompositeDriverConfigLoader(
+      @NonNull DriverConfigLoader primaryConfigLoader,
+      @NonNull DriverConfigLoader fallbackConfigLoader) {
+    this.primaryConfigLoader = Objects.requireNonNull(primaryConfigLoader);
+    this.fallbackConfigLoader = Objects.requireNonNull(fallbackConfigLoader);
+  }
+
+  @NonNull
+  @Override
+  public DriverConfig getInitialConfig() {
+    DriverConfig primaryConfig = primaryConfigLoader.getInitialConfig();
+    DriverConfig fallbackConfig = fallbackConfigLoader.getInitialConfig();
+    return new CompositeDriverConfig(primaryConfig, fallbackConfig);
+  }
+
+  @Override
+  public void onDriverInit(@NonNull DriverContext context) {
+    fallbackConfigLoader.onDriverInit(context);
+    primaryConfigLoader.onDriverInit(context);
+  }
+
+  @NonNull
+  @Override
+  public CompletionStage<Boolean> reload() {
+    if (!primaryConfigLoader.supportsReloading() && !fallbackConfigLoader.supportsReloading()) {
+      return CompletableFutures.failedFuture(
+          new UnsupportedOperationException(
+              "Reloading is not supported (this is a composite config, "
+                  + "and neither the primary nor the fallback are reloadable)"));
+    } else if (!primaryConfigLoader.supportsReloading()) {
+      return fallbackConfigLoader.reload();
+    } else if (!fallbackConfigLoader.supportsReloading()) {
+      return primaryConfigLoader.reload();
+    } else {
+      CompletionStage<Boolean> primaryFuture = primaryConfigLoader.reload();
+      CompletionStage<Boolean> fallbackFuture = fallbackConfigLoader.reload();
+      CompletableFuture<Boolean> compositeFuture = new CompletableFuture<>();
+      primaryFuture.whenComplete(
+          (primaryChanged, primaryError) ->
+              fallbackFuture.whenComplete(
+                  (fallbackChanged, fallbackError) -> {
+                    if (primaryError == null && fallbackError == null) {
+                      compositeFuture.complete(primaryChanged || fallbackChanged);
+                    } else if (fallbackError == null) {
+                      compositeFuture.completeExceptionally(primaryError);
+                    } else if (primaryError == null) {
+                      compositeFuture.completeExceptionally(fallbackError);
+                    } else {
+                      primaryError.addSuppressed(fallbackError);
+                      compositeFuture.completeExceptionally(primaryError);
+                    }
+                  }));
+      return compositeFuture;
+    }
+  }
+
+  @Override
+  public boolean supportsReloading() {
+    return primaryConfigLoader.supportsReloading() || fallbackConfigLoader.supportsReloading();
+  }
+
+  @Override
+  public void close() {
+    primaryConfigLoader.close();
+    fallbackConfigLoader.close();
+  }
+}
---
+++ core/src/test/java/com/datastax/oss/driver/internal/core/config/composite/CompositeDriverConfigTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.internal.core.config.composite;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
+import com.datastax.oss.driver.api.core.config.DriverConfig;
+import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
+import com.datastax.oss.driver.api.core.config.DriverExecutionProfile;
+import com.datastax.oss.driver.api.core.config.OptionsMap;
+import com.datastax.oss.driver.api.core.config.TypedDriverOption;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CompositeDriverConfigTest {
+
+  private OptionsMap primaryMap;
+  private OptionsMap fallbackMap;
+  private DriverConfig compositeConfig;
+  private DriverExecutionProfile compositeDefaultProfile;
+
+  @Before
+  public void setup() {
+    primaryMap = new OptionsMap();
+    // We need at least one option so that the default profile exists. Do it now to avoid having to
+    // do it in every test. We use an option that we won't reuse in the tests so that there are no
+    // unwanted interactions.
+    primaryMap.put(TypedDriverOption.CONTINUOUS_PAGING_MAX_PAGES, 1);
+
+    fallbackMap = new OptionsMap();
+    fallbackMap.put(TypedDriverOption.CONTINUOUS_PAGING_MAX_PAGES, 1);
+
+    DriverConfigLoader compositeLoader =
+        DriverConfigLoader.compose(
+            DriverConfigLoader.fromMap(primaryMap), DriverConfigLoader.fromMap(fallbackMap));
+    compositeConfig = compositeLoader.getInitialConfig();
+    compositeDefaultProfile = compositeConfig.getDefaultProfile();
+  }
+
+  @Test
+  public void should_use_value_from_primary_config() {
+    primaryMap.put(TypedDriverOption.CONNECTION_POOL_LOCAL_SIZE, 1);
+
+    assertThat(compositeDefaultProfile.isDefined(DefaultDriverOption.CONNECTION_POOL_LOCAL_SIZE))
+        .isTrue();
+    assertThat(compositeDefaultProfile.getInt(DefaultDriverOption.CONNECTION_POOL_LOCAL_SIZE))
+        .isEqualTo(1);
+  }
+
+  @Test
+  public void should_ignore_value_from_fallback_config_if_defined_in_both() {
+    primaryMap.put(TypedDriverOption.CONNECTION_POOL_LOCAL_SIZE, 1);
+    fallbackMap.put(TypedDriverOption.CONNECTION_POOL_LOCAL_SIZE, 2);
+
+    assertThat(compositeDefaultProfile.isDefined(DefaultDriverOption.CONNECTION_POOL_LOCAL_SIZE))
+        .isTrue();
+    assertThat(compositeDefaultProfile.getInt(DefaultDriverOption.CONNECTION_POOL_LOCAL_SIZE))
+        .isEqualTo(1);
+  }
+
+  @Test
+  public void should_use_value_from_fallback_config_if_not_defined_in_primary() {
+    fallbackMap.put(TypedDriverOption.CONNECTION_POOL_LOCAL_SIZE, 1);
+
+    assertThat(compositeDefaultProfile.isDefined(DefaultDriverOption.CONNECTION_POOL_LOCAL_SIZE))
+        .isTrue();
+    assertThat(compositeDefaultProfile.getInt(DefaultDriverOption.CONNECTION_POOL_LOCAL_SIZE))
+        .isEqualTo(1);
+  }
+
+  @Test
+  public void should_merge_profiles() {
+    primaryMap.put("onlyInPrimary", TypedDriverOption.CONNECTION_POOL_LOCAL_SIZE, 1);
+    primaryMap.put("inBoth", TypedDriverOption.CONNECTION_POOL_LOCAL_SIZE, 2);
+    fallbackMap.put("inBoth", TypedDriverOption.CONNECTION_POOL_LOCAL_SIZE, 3);
+    fallbackMap.put("onlyInFallback", TypedDriverOption.CONNECTION_POOL_LOCAL_SIZE, 4);
+
+    assertThat(compositeConfig.getProfiles())
+        .containsKeys(
+            DriverExecutionProfile.DEFAULT_NAME,
+            "onlyInPrimary",
+            "inBoth",
+            "inBoth",
+            "onlyInFallback");
+
+    assertThat(
+            compositeConfig
+                .getProfile("onlyInPrimary")
+                .getInt(DefaultDriverOption.CONNECTION_POOL_LOCAL_SIZE))
+        .isEqualTo(1);
+    assertThat(
+            compositeConfig
+                .getProfile("inBoth")
+                .getInt(DefaultDriverOption.CONNECTION_POOL_LOCAL_SIZE))
+        .isEqualTo(2);
+    assertThat(
+            compositeConfig
+                .getProfile("onlyInFallback")
+                .getInt(DefaultDriverOption.CONNECTION_POOL_LOCAL_SIZE))
+        .isEqualTo(4);
+  }
+}
