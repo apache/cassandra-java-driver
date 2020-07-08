c457970f38597552d62abf9eb724e530ee2d98f1
olim7t <omichallat+github@gmail.com>
2020-07-08 10:32:22 -0700

    JAVA-2431: Set all occurrences when bound variables are used multiple times

--- core/src/main/java/com/datastax/oss/driver/internal/core/cql/DefaultBoundStatement.java
+++ core/src/main/java/com/datastax/oss/driver/internal/core/cql/DefaultBoundStatement.java
@@ -117,6 +117,16 @@ public class DefaultBoundStatement implements BoundStatement {
     return variableDefinitions.get(i).getType();
   }
 
+  @NonNull
+  @Override
+  public List<Integer> allIndicesOf(@NonNull CqlIdentifier id) {
+    List<Integer> indices = variableDefinitions.allIndicesOf(id);
+    if (indices.isEmpty()) {
+      throw new IllegalArgumentException(id + " is not a variable in this bound statement");
+    }
+    return indices;
+  }
+
   @Override
   public int firstIndexOf(@NonNull CqlIdentifier id) {
     int indexOf = variableDefinitions.firstIndexOf(id);
@@ -124,6 +134,16 @@ public class DefaultBoundStatement implements BoundStatement {
       throw new IllegalArgumentException(id + " is not a variable in this bound statement");
     }
     return indexOf;
+  }
+
+  @NonNull
+  @Override
+  public List<Integer> allIndicesOf(@NonNull String name) {
+    List<Integer> indices = variableDefinitions.allIndicesOf(name);
+    if (indices.isEmpty()) {
+      throw new IllegalArgumentException(name + " is not a variable in this bound statement");
+    }
+    return indices;
   }
 
   @Override
