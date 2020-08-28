9032edc98e36a30f60d0f933047df9f538479d34
olim7t <omichallat+github@gmail.com>
2020-08-28 12:28:50 -0700

    Fix capitalization rules to infer setter name in mapper

--- mapper-processor/src/main/java/com/datastax/oss/driver/internal/mapper/processor/util/Capitalizer.java
+++ mapper-processor/src/main/java/com/datastax/oss/driver/internal/mapper/processor/util/Capitalizer.java
@@ -20,13 +20,34 @@ import java.util.Objects;
 
 public class Capitalizer {
 
+  /**
+   * Lower cases the first character of a name, for example when inferring the name of a field from
+   * the name of a getter stripped of its {@code get} prefix.
+   *
+   * <p>This method respects a weird corner case of the JavaBeans conventions: "in the (unusual)
+   * special case when there is more than one character and both the first and second characters are
+   * upper case, we leave it alone. Thus {@code FooBah} becomes {@code fooBah} and {@code X} becomes
+   * {@code x}, but {@code URL} stays as {@code URL}.".
+   */
   public static String decapitalize(String name) {
     return Introspector.decapitalize(Objects.requireNonNull(name));
   }
 
+  /**
+   * Upper cases the first character of a name, for example when inferring the name of a setter from
+   * the name of a field.
+   *
+   * <p>Mirroring the behavior of {@link #decapitalize(String)}, this method returns the string
+   * unchanged not only if the first character is uppercase, but also if the <em>second</em> is. For
+   * example, if a field is named {@code cId}, we want to produce the setter name {@code setcId()},
+   * not {@code setCId()}. Otherwise applying the process in reverse would produce the field name
+   * {@code CId}.
+   */
   public static String capitalize(String name) {
     Objects.requireNonNull(name);
-    if (name.isEmpty() || Character.isUpperCase(name.charAt(0))) {
+    if (name.isEmpty()
+        || Character.isUpperCase(name.charAt(0))
+        || (name.length() > 1 && Character.isUpperCase(name.charAt(1)))) {
       return name;
     } else {
       char[] chars = name.toCharArray();
