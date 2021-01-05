RobertoMalatesta/java-driver

JAVA-2899: Re-introduce cross-DC failover in driver 4

core/src/main/java/com/datastax/dse/driver/internal/core/graph/GraphRequestHandler.java
@@ -59,7 +59,7 @@ import com.datastax.oss.driver.internal.core.session.DefaultSession;
 import com.datastax.oss.driver.internal.core.tracker.NoopRequestTracker;
 import com.datastax.oss.driver.internal.core.tracker.RequestLogger;
 import com.datastax.oss.driver.internal.core.util.Loggers;
-import com.datastax.oss.driver.internal.core.util.collection.QueryPlan;
+import com.datastax.oss.driver.internal.core.util.collection.SimpleQueryPlan;
 import com.datastax.oss.protocol.internal.Frame;
 import com.datastax.oss.protocol.internal.Message;
 import com.datastax.oss.protocol.internal.response.Error;
@@ -216,7 +216,7 @@ public class GraphRequestHandler implements Throttled {
     }
     Queue<Node> queryPlan =
         statement.getNode() != null
-            ? new QueryPlan(statement.getNode())
+            ? new SimpleQueryPlan(statement.getNode())
             : context
                 .getLoadBalancingPolicyWrapper()
                 .newQueryPlan(statement, executionProfile.getName(), session);

core/src/main/java/com/datastax/oss/driver/internal/core/cql/CqlRequestHandler.java
@@ -56,7 +56,7 @@ import com.datastax.oss.driver.internal.core.session.RepreparePayload;
 import com.datastax.oss.driver.internal.core.tracker.NoopRequestTracker;
 import com.datastax.oss.driver.internal.core.tracker.RequestLogger;
 import com.datastax.oss.driver.internal.core.util.Loggers;
-import com.datastax.oss.driver.internal.core.util.collection.QueryPlan;
+import com.datastax.oss.driver.internal.core.util.collection.SimpleQueryPlan;
 import com.datastax.oss.protocol.internal.Frame;
 import com.datastax.oss.protocol.internal.Message;
 import com.datastax.oss.protocol.internal.ProtocolConstants;
@@ -205,7 +205,7 @@ public class CqlRequestHandler implements Throttled {
     }
     Queue<Node> queryPlan =
         this.statement.getNode() != null
-            ? new QueryPlan(this.statement.getNode())
+            ? new SimpleQueryPlan(this.statement.getNode())
             : context
                 .getLoadBalancingPolicyWrapper()
                 .newQueryPlan(statement, executionProfile.getName(), session);

apache/cassandra-java-driver

core/src/main/java/com/datastax/dse/driver/internal/core/graph/GraphRequestHandler.java
@@ -57,7 +57,7 @@ import com.datastax.oss.driver.internal.core.session.DefaultSession;
 import com.datastax.oss.driver.internal.core.tracker.NoopRequestTracker;
 import com.datastax.oss.driver.internal.core.tracker.RequestLogger;
 import com.datastax.oss.driver.internal.core.util.Loggers;
-import com.datastax.oss.driver.internal.core.util.collection.QueryPlan;
+import com.datastax.oss.driver.internal.core.util.collection.SimpleQueryPlan;
 import com.datastax.oss.protocol.internal.Frame;
 import com.datastax.oss.protocol.internal.Message;
 import com.datastax.oss.protocol.internal.response.Error;
@@ -190,7 +190,7 @@ public class GraphRequestHandler implements Throttled {
     }
     Queue<Node> queryPlan =
         initialStatement.getNode() != null
-            ? new QueryPlan(initialStatement.getNode())
+            ? new SimpleQueryPlan(initialStatement.getNode())
             : context
                 .getLoadBalancingPolicyWrapper()
                 .newQueryPlan(initialStatement, executionProfile.getName(), session);

core/src/main/java/com/datastax/oss/driver/internal/core/cql/CqlRequestHandler.java
@@ -55,7 +55,7 @@ import com.datastax.oss.driver.internal.core.session.RepreparePayload;
 import com.datastax.oss.driver.internal.core.tracker.NoopRequestTracker;
 import com.datastax.oss.driver.internal.core.tracker.RequestLogger;
 import com.datastax.oss.driver.internal.core.util.Loggers;
-import com.datastax.oss.driver.internal.core.util.collection.QueryPlan;
+import com.datastax.oss.driver.internal.core.util.collection.SimpleQueryPlan;
 import com.datastax.oss.protocol.internal.Frame;
 import com.datastax.oss.protocol.internal.Message;
 import com.datastax.oss.protocol.internal.ProtocolConstants;
@@ -187,7 +187,7 @@ public class CqlRequestHandler implements Throttled {
     }
     Queue<Node> queryPlan =
         this.initialStatement.getNode() != null
-            ? new QueryPlan(this.initialStatement.getNode())
+            ? new SimpleQueryPlan(this.initialStatement.getNode())
             : context
                 .getLoadBalancingPolicyWrapper()
                 .newQueryPlan(initialStatement, executionProfile.getName(), session);