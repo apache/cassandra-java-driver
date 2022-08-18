cd4212861fb590013ad326701e23631975588a3a
Sergey Matvienko <smatvienko@thingsboard.io>
2022-08-18 09:41:36 +0300

    Guava session - cloud bundle support. replaced deprecated constructors

--- integration-tests/src/test/java/com/datastax/oss/driver/example/guava/api/GuavaSessionBuilder.java
+++ integration-tests/src/test/java/com/datastax/oss/driver/example/guava/api/GuavaSessionBuilder.java
@@ -18,40 +18,17 @@ package com.datastax.oss.driver.example.guava.api;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.context.DriverContext;
-import com.datastax.oss.driver.api.core.metadata.Node;
-import com.datastax.oss.driver.api.core.metadata.NodeStateListener;
-import com.datastax.oss.driver.api.core.metadata.schema.SchemaChangeListener;
+import com.datastax.oss.driver.api.core.session.ProgrammaticArguments;
 import com.datastax.oss.driver.api.core.session.SessionBuilder;
-import com.datastax.oss.driver.api.core.tracker.RequestTracker;
-import com.datastax.oss.driver.api.core.type.codec.TypeCodec;
 import com.datastax.oss.driver.example.guava.internal.DefaultGuavaSession;
 import com.datastax.oss.driver.example.guava.internal.GuavaDriverContext;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
 
 public class GuavaSessionBuilder extends SessionBuilder<GuavaSessionBuilder, GuavaSession> {
 
   @Override
-  protected DriverContext buildContext(
-      DriverConfigLoader configLoader,
-      List<TypeCodec<?>> typeCodecs,
-      NodeStateListener nodeStateListener,
-      SchemaChangeListener schemaChangeListener,
-      RequestTracker requestTracker,
-      Map<String, String> localDatacenters,
-      Map<String, Predicate<Node>> nodeFilters,
-      ClassLoader classLoader) {
-    return new GuavaDriverContext(
-        configLoader,
-        typeCodecs,
-        nodeStateListener,
-        schemaChangeListener,
-        requestTracker,
-        localDatacenters,
-        nodeFilters,
-        classLoader);
+  protected DriverContext buildContext(DriverConfigLoader configLoader, ProgrammaticArguments programmaticArguments) {
+    return new GuavaDriverContext(configLoader, programmaticArguments);
   }
 
   @Override
--- integration-tests/src/test/java/com/datastax/oss/driver/example/guava/internal/GuavaDriverContext.java
+++ integration-tests/src/test/java/com/datastax/oss/driver/example/guava/internal/GuavaDriverContext.java
@@ -18,11 +18,7 @@ package com.datastax.oss.driver.example.guava.internal;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.cql.PrepareRequest;
 import com.datastax.oss.driver.api.core.cql.Statement;
-import com.datastax.oss.driver.api.core.metadata.Node;
-import com.datastax.oss.driver.api.core.metadata.NodeStateListener;
-import com.datastax.oss.driver.api.core.metadata.schema.SchemaChangeListener;
-import com.datastax.oss.driver.api.core.tracker.RequestTracker;
-import com.datastax.oss.driver.api.core.type.codec.TypeCodec;
+import com.datastax.oss.driver.api.core.session.ProgrammaticArguments;
 import com.datastax.oss.driver.example.guava.api.GuavaSession;
 import com.datastax.oss.driver.internal.core.context.DefaultDriverContext;
 import com.datastax.oss.driver.internal.core.cql.CqlPrepareAsyncProcessor;
@@ -30,9 +26,6 @@ import com.datastax.oss.driver.internal.core.cql.CqlPrepareSyncProcessor;
 import com.datastax.oss.driver.internal.core.cql.CqlRequestAsyncProcessor;
 import com.datastax.oss.driver.internal.core.cql.CqlRequestSyncProcessor;
 import com.datastax.oss.driver.internal.core.session.RequestProcessorRegistry;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
 
 /**
  * A Custom {@link DefaultDriverContext} that overrides {@link #getRequestProcessorRegistry()} to
@@ -40,24 +33,8 @@ import java.util.function.Predicate;
  */
 public class GuavaDriverContext extends DefaultDriverContext {
 
-  public GuavaDriverContext(
-      DriverConfigLoader configLoader,
-      List<TypeCodec<?>> typeCodecs,
-      NodeStateListener nodeStateListener,
-      SchemaChangeListener schemaChangeListener,
-      RequestTracker requestTracker,
-      Map<String, String> localDatacenters,
-      Map<String, Predicate<Node>> nodeFilters,
-      ClassLoader classLoader) {
-    super(
-        configLoader,
-        typeCodecs,
-        nodeStateListener,
-        schemaChangeListener,
-        requestTracker,
-        localDatacenters,
-        nodeFilters,
-        classLoader);
+  public GuavaDriverContext(DriverConfigLoader configLoader, ProgrammaticArguments programmaticArguments) {
+    super(configLoader, programmaticArguments);
   }
 
   @Override
