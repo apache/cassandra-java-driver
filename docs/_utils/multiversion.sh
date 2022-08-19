e23a1fa4d374443910a4e8be6576ed680d09f13e
David Garcia <hi@davidgarcia.dev>
2022-08-19 18:00:39 +0100

    docs: update theme 1.3
    (cherry picked from commit d2e261bcf437bd4863ce22215fdf5a4bfbf1f7de)

--- docs/_utils/multiversion.sh
+++ docs/_utils/multiversion.sh
@@ -1,5 +1,5 @@
 #! /bin/bash	
 
 cd .. && sphinx-multiversion docs/source docs/_build/dirhtml \
-    --pre-build './docs/_utils/javadoc.sh' \
-    --pre-build "find . -mindepth 2 -name README.md -execdir mv '{}' index.md ';'"
+    --pre-build "find . -mindepth 2 -name README.md -execdir mv '{}' index.md ';'" \
+    --post-build './docs/_utils/javadoc.sh'
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ LATEST_VERSION = 'scylla-4.13.0.x'
 # Set which versions are not released yet.
 UNSTABLE_VERSIONS = []
 # Set which versions are deprecated
-DEPRECATED_VERSIONS = ['']
+DEPRECATED_VERSIONS = []
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -116,11 +116,6 @@ notfound_template =  '404.html'
 
 # Prefix added to all the URLs generated in the 404 page.
 notfound_urls_prefix = ''
-
-# -- Options for redirect extension ---------------------------------------
-
-# Read a YAML dictionary of redirections and generate an HTML file for each
-redirects_file = "_utils/redirections.yaml"
 
 # -- Options for multiversion extension ----------------------------------
 
