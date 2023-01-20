9e6cd678d0702cb07bab7195ab06a733c31452a3
David Garcia <hi@davidgarcia.dev>
2023-01-20 18:04:30 +0000

    Add license notice
    (cherry picked from commit 7399a0ad40999f203639330d88ebaf99db6a0d10)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,9 @@ extensions = [
     'sphinx_multiversion',
 ]
 
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
