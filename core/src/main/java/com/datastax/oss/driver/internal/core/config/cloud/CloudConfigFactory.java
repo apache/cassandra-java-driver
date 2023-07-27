f292c60f1394cdb61e1f526e4f4bce71401761ad
Henry Hughes <henry.hughes@datastax.com>
2023-07-27 15:21:09 -0700

    Small fixes to initial commit - Move FNF handling to before IOE (FNF is subclass of IOE) - Update tests to assert exact message strings - add test for bad gateway (502) from /metadata - remove wildcard exceptions

--- core/src/main/java/com/datastax/oss/driver/internal/core/config/cloud/CloudConfigFactory.java
+++ core/src/main/java/com/datastax/oss/driver/internal/core/config/cloud/CloudConfigFactory.java
@@ -24,7 +24,13 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
@@ -226,6 +232,10 @@ public class CloudConfigFactory {
       try {
         return new BufferedReader(
             new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+      } catch (FileNotFoundException e) {
+        throw new IllegalStateException(
+            "Metadata service request path not found. Please make sure the metadata service url is correct",
+            e);
       } catch (IOException ioe) {
         throw new IllegalStateException(
             "Unable to read data from cloud metadata service. Please make sure your cluster is not parked or terminated",
@@ -238,10 +248,6 @@ public class CloudConfigFactory {
     } catch (UnknownHostException e) {
       throw new IllegalStateException(
           "Unable to resolve host for cloud metadata service. Please make sure your cluster is not terminated",
-          e);
-    } catch (FileNotFoundException e) {
-      throw new IllegalStateException(
-          "Metadata service request path not found. Please make sure the metadata service url is correct",
           e);
     }
   }
