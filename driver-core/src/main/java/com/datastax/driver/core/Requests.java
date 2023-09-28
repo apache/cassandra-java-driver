a13df433333c547c55724e94f095b22a3816b8f9
mck <mck@apache.org>
2023-09-28 21:53:40 +0200

    Copyright to ASF (see https://incubator.apache.org/ip-clearance/cassandra-java-driver.html )
     patch by Mick Semb Wever; reviewed by Henry Hughes for CASSANDRA-18611

--- driver-core/src/main/java/com/datastax/driver/core/Requests.java
+++ driver-core/src/main/java/com/datastax/driver/core/Requests.java
@@ -1,11 +1,13 @@
 /*
- * Copyright DataStax, Inc.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -36,7 +38,7 @@ class Requests {
     private static final String CQL_VERSION = "3.0.0";
     private static final String DRIVER_VERSION_OPTION = "DRIVER_VERSION";
     private static final String DRIVER_NAME_OPTION = "DRIVER_NAME";
-    private static final String DRIVER_NAME = "DataStax Java Driver";
+    private static final String DRIVER_NAME = "Java Driver";
 
     static final String COMPRESSION_OPTION = "COMPRESSION";
     static final String NO_COMPACT_OPTION = "NO_COMPACT";
--- driver-core/src/main/java/com/datastax/driver/core/exceptions/package-info.java
+++ driver-core/src/main/java/com/datastax/driver/core/exceptions/package-info.java
@@ -1,11 +1,13 @@
 /*
- * Copyright DataStax, Inc.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,5 +15,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Exceptions thrown by the DataStax Java driver for Cassandra. */
+/** Exceptions thrown by the Java Driver for Cassandra. */
 package com.datastax.driver.core.exceptions;
