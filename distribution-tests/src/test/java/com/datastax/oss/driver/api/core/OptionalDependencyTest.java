d50905b888cf90412e8a120482eca0bfaa7bc470
Henry Hughes <henry.hughes@datastax.com>
2023-11-01 12:50:18 -0700

    Mark spotbugs-annotation and jcip-annotations as provided dependencies [4.2.x branch]
    patch by Henry Hughes; reviewed by Mick Semb Wever for CASSANDRA-18969

---
+++ distribution-tests/src/test/java/com/datastax/oss/driver/api/core/OptionalDependencyTest.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.driver.api.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.datastax.oss.driver.internal.core.util.Reflection;
+import org.junit.Test;
+
+public class OptionalDependencyTest {
+  @Test
+  public void should_not_include_snappy_jar() {
+    assertThat(Reflection.loadClass(null, "org.xerial.snappy.Snappy")).isNull();
+  }
+
+  @Test
+  public void should_not_include_l4z_jar() {
+    assertThat(Reflection.loadClass(null, "net.jpountz.lz4.LZ4Factory")).isNull();
+  }
+}
