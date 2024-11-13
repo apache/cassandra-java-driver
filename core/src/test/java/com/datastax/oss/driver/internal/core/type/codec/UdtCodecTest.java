ef9bed9b5d07f3c7018f489417235be84d6b30c4
Dmitry Konstantinov <netudima@gmail.com>
2024-11-13 18:50:47 +0000

    Fix formatting issue
    Patch by Dmitry Konstantinov; reviewed by TBD for CASSANDRA-19814

--- core/src/test/java/com/datastax/oss/driver/internal/core/type/codec/UdtCodecTest.java
+++ core/src/test/java/com/datastax/oss/driver/internal/core/type/codec/UdtCodecTest.java
@@ -137,18 +137,21 @@ public class UdtCodecTest extends CodecTestBase<UdtValue> {
 
   @Test
   public void should_decode_udt_when_too_many_fields() {
-    UdtValue udt = decode("0x"
-            + ("00000004" + "00000001") // size and contents of int field 0
-            + "ffffffff" // null field 1
-            + ("00000001" + "61") // size and contents of String field 2
-            + ("00000004" + "00000002") // size and contents of int field 3, unknown for UDT version used to create the codec
-    );
+    UdtValue udt =
+        decode(
+            "0x"
+                + ("00000004" + "00000001") // size and contents of int field 0
+                + "ffffffff" // null field 1
+                + ("00000001" + "61") // size and contents of String field 2
+                + ("00000004"
+                    + "00000002") // size and contents of int field 3, unknown for UDT version used
+            // to create the codec
+            );
 
     assertThat(udt.getInt(0)).isEqualTo(1);
     assertThat(udt.isNull(1)).isTrue();
     assertThat(udt.getString(2)).isEqualTo("a");
     assertThat(udt.size()).isEqualTo(3); // unknown field is not decoded
-
 
     verify(intCodec).decodePrimitive(Bytes.fromHexString("0x00000001"), ProtocolVersion.DEFAULT);
     verifyZeroInteractions(doubleCodec);
