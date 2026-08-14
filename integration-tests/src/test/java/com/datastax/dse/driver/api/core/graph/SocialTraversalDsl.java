b4e00ed7f52e484a00fa588d6a5d8fb659bf7edc
absurdfarce <bret.mcguire@ibm.com>
2026-08-14 16:05:09 -0500

    CASSJAVA-135 Update the build to use Java17 rather than Java8
    patch by Bret McGuire; reviewed by Jane He

--- integration-tests/src/test/java/com/datastax/dse/driver/api/core/graph/SocialTraversalDsl.java
+++ integration-tests/src/test/java/com/datastax/dse/driver/api/core/graph/SocialTraversalDsl.java
@@ -17,11 +17,9 @@
  */
 package com.datastax.dse.driver.api.core.graph;
 
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.GremlinDsl;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 
-@GremlinDsl(traversalSource = "com.datastax.dse.driver.api.core.graph.SocialTraversalSourceDsl")
 public interface SocialTraversalDsl<S, E> extends GraphTraversal.Admin<S, E> {
   public default GraphTraversal<S, Vertex> knows(String personName) {
     return out("knows").hasLabel("person").has("name", personName).in();
