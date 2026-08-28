323476070439078e55b2ca6b6cfb4ad3bd266042
Shanzita <shanzitasiddiqua@gmail.com>
2026-08-28 02:18:25 -0500

    CASSJAVA-124: Revert CI and snapshot-install changes
    Per review: install-snapshots.sh is obsolete and CI will pass once native-protocol 1.5.3 is released, so these files go back to trunk as-is.

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -4,11 +4,7 @@
 . ~/env.txt
 cd $(dirname "$(readlink -f "$0")")/..
 printenv | sort
-# Install snapshot dependencies (e.g. native-protocol) that are not published to a public repo yet.
-# Fail the build immediately if this does not succeed, and pass -U below so that any stale
-# resolution-failure markers in the local repository do not block the freshly installed snapshot.
-./install-snapshots.sh || exit 1
-mvn -U -B -V install -DskipTests -Dmaven.javadoc.skip=true
+mvn -B -V install -DskipTests -Dmaven.javadoc.skip=true
 jabba use ${TEST_JAVA_VERSION}
 # Find out the latest patch version of Cassandra
 PATCH_SERVER_VERSION=$(curl -s https://downloads.apache.org/cassandra/ | grep -oP '(?<=href=\")[0-9]+\.[0-9]+\.[0-9]+(?=)' | sort -rV | uniq -w 3 | grep $SERVER_VERSION)
--- install-snapshots.sh
+++ install-snapshots.sh
@@ -6,9 +6,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-#
+# 
 #   http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -16,32 +16,26 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# Install snapshot dependencies that are not published to a public repository yet.
-#
-# The driver currently depends on native-protocol 1.5.3-SNAPSHOT (CEP-59 protocol types),
-# which only exists on the branch behind datastax/native-protocol PR #61. Install it
-# unconditionally: a probing "is the dependency a snapshot?" mvn run would write a
-# resolution-failure marker into the local repository, which then blocks the main build
-# even after the snapshot is installed.
-#
-# TODO: remove this script's invocation from ci/run-tests.sh (and revert this file to
-# cloning https://github.com/datastax/native-protocol.git) once native-protocol 1.5.3
-# is released.
+# Install dependencies in the Travis build environment if they are snapshots.
+# See .travis.yml
 
-set -eu
+set -u
 
 install_snapshot()
 {
   URL=$1
-  BRANCH=$2
-  # Clone into a unique directory so concurrent builds on the same host cannot collide.
-  CLONE_DIR=$(mktemp -d)/$(basename ${URL} .git)
-  git clone --depth 1 --branch ${BRANCH} ${URL} ${CLONE_DIR}
-  (
-    cd ${CLONE_DIR}
-    mvn -B install -DskipTests
-  )
-  rm -rf ${CLONE_DIR}
+  DIRECTORY_NAME=$2
+  # Assume the snapshot we want is on the head of the default branch
+  git clone --depth 1 ${URL} /tmp/${DIRECTORY_NAME}
+  {
+    cd /tmp/${DIRECTORY_NAME}
+    mvn install -DskipTests
+  }
 }
 
-install_snapshot https://github.com/Shanzita/native-protocol.git cep-59
+mvn --projects core dependency:list -DincludeArtifactIds=native-protocol | \
+  tee /dev/tty | \
+  grep -q native-protocol.*SNAPSHOT
+if [ $? -eq 0 ] ; then
+  install_snapshot https://github.com/datastax/native-protocol.git native-protocol
+fi
